2f23ebf0879366b6a710f0c51b5dcb87dcb31a66
jackysee <jackysee@gmail.com>
2020-12-11 10:24:23 +0800

    cleanup

--- 2019/day10.js
+++ 2019/day10.js
@@ -9,58 +9,6 @@ function parseMap(str) {
     }
 }
 
-function gcd(n1, n2) {
-    if(n1 === 0 || n2 === 0) {
-        return 0;
-    }
-    while(n1 != n2){
-        if(n1 > n2) {
-            n1 -= n2;
-        } else {
-            n2 -= n1;
-        }
-    }
-    return n1;
-}
-
-function getPointsBetween([x1,y1],[x2,y2]){
-    let dx = x2 - x1;
-    let dy = y2 - y1;
-    let f = gcd(Math.abs(dx), Math.abs(dy));
-    if(f !== 0) {
-        dx = dx / f;
-        dy = dy / f;
-    }
-    if(dx === 0) {
-        dy = dy < 0? -1 : 1;
-    }
-    if(dy === 0) {
-        dx = dx < 0? -1 : 1;
-    }
-    let points = [], x = x1, y = y1;
-    while(!(x === x2 && y === y2)) {
-        points.push([x, y]);
-        x += dx;
-        y += dy;
-    }
-    return points;
-}
-
-function countAsteroids2(x,y,map) {
-    let num = 0;
-    for(let j=0; j<map.height; j++) {
-        for(let i=0; i<map.width; i++) {
-            if(map.at(i,j) === '#') {
-                if(getPointsBetween([i,j], [x,y]).filter(p => map.at(...p) === '#').length === 1) {
-                    num++;
-                }
-            }
-        }
-    }
-    return num;
-}
-
-
 function getAngle(dx,dy) {
    let r = - Math.atan2(dy,dx) + 90 * Math.PI  / 180;
    return r < 0 ? 2 * Math.PI + r : r;
@@ -100,8 +48,6 @@ function findBest(map) {
     return result;
 }
 
-// console.log(getPointsBetween([9,8],[5,8]));
-// console.log(getTargetPoints(2,2,{ width:6, height:3}));
 const map = parseMap(data());
 const best = findBest(map);
 console.log(best);
