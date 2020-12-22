456d5be6cf7ef93ba32a861e4f7e1b2c7f07d775
jackysee <jackysee@gmail.com>
2020-12-22 10:16:49 +0800

    2020 d21

--- 2020/day21.js
+++ 2020/day21.js
@@ -10,20 +10,48 @@ function parse(str) {
     });
 }
 
-console.log(parse(sample()));
-
-function part1(str) {
-    let data = parse(str);
+function findAllerg(data) {
     let map = {};
     data.forEach(d => {
         d.allerg.forEach(a => {
-            map[a] = map[a] || new Set();
-            d.ingred.forEach(i => {
-
-            });
+            map[a] = new Set(
+                map[a] ? d.ingred.filter(_d => map[a].has(_d)) : d.ingred
+            );
+            for(let j in map) {
+                if(map[j].size === 1)  {
+                    for(let k in map) {
+                        if(j !== k) {
+                            map[k].delete([...map[j]][0]);
+                        }
+                    }
+                }
+            }
         });
     });
+    return map;
 }
+
+function part1(str) {
+    let data = parse(str);
+    let map = findAllerg(data);
+    const allAllerg = new Set(Object.values(map).flatMap(s => [...s]));
+    return data.reduce((a, d) => a + d.ingred.filter(i => !allAllerg.has(i)).length, 0);
+}
+
+function part2(str) {
+    let data = parse(str);
+    let map = findAllerg(data);
+    let ingred = Object.entries(map).sort(([k1], [k2]) => {
+        if(k1 < k2) return -1;
+        if(k1 > k2) return 1;
+        return 0;
+    }).map(([k, v]) => [...v][0]);
+    return ingred.join(',');
+}
+
+console.log(part1(data()));
+console.log(part2(data()));
+
 
 
 function sample() {
