5c3f20db66c3f20056965a8823c98e2bdfbd5eca
JackySee <jackysee@gmail.com>
2020-12-23 05:50:42 +0000

    Wip

--- 2020/day23.js
+++ 2020/day23.js
@@ -1,14 +1,28 @@
 //AOC2020 D22
 
 function parse(str) {
-    return str.split('').map(s => parseInt(s, 10));
+	let arr = str.split('').map(s => parseInt(s, 10));
+	return arr.map((n, i) => {
+	        return {
+	            val: n,
+	            next: arr[i+1 === arr.length? 0 : i+1]
+	        }
+	    });
+	
 }
 
+let curr = 6;
+
+function move() {
+    arr
+}
+
+console.log(parse(data()));
 
 function sample() {
-    return ``;
+	return ``;
 }
 
 function data() {
-    return `643719258`;
+	return `643719258`;
 }
