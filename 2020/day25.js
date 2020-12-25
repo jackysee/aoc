a538b8a23f1072964675fca91f18dcf935d9a7ce
jackysee <jackysee@gmail.com>
2020-12-25 23:38:06 +0800

    refactor

--- 2020/day25.js
+++ 2020/day25.js
@@ -3,32 +3,24 @@ let door = 7573546;
 let card = 17786549;
 
 function loopSize(target) {
-    let val = 1;
-    let i = 0;
-    while(true) {
+    let val = 1, i = 0;
+    while(val !== target) {
         i++;
         val = val * 7 % 20201227;
-        if(val === target) {
-            break;
-        }
     }
     return i;
 }
 
 function transform(key, loopSize) {
-    let val = 1;
-    let i = 0;
+    let val = 1, i = 0;
     while(i < loopSize) {
         i++;
-        let _val = val * key;
         val = val * key % 20201227;
     }
     return val;
 }
 
 // console.log(transform(17807724, findLoopSize(5764801))); //14897079
-
 console.log(transform(door, loopSize(card)));
-// console.log(transform(card, doorLoopSize));
 
 
