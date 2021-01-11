177b797c8ada0e7cd1e12cd101b7771db382f4ff
jackysee <jackysee@gmail.com>
2021-01-11 22:47:54 +0800

    2019 d24 wip

--- 2019/day24.js
+++ 2019/day24.js
@@ -2,6 +2,36 @@
 
 function parse(str) {
     let map = str.split('\n').map(l => l.split(''));
+    return map;
+}
+
+function rating(map) {
+    let k = 0, sum = 0;
+    for(let i=0; i<5; i++) {
+        for(let j=0; j<5; j++) {
+            if(map[i][j] === '#') {
+                sum += Math.pow(2, k)
+            }
+            k++;
+        }
+    }
+    return sum;
+}
+
+function part1(str) {
+    let map = parse(str);
+}
+
+console.log(rating(parse(sample())));
+
+function sample() {
+    return `
+.....
+.....
+.....
+#....
+.#...
+    `.trim();
 }
 
 function data() {
