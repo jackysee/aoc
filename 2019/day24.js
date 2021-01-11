3aeaa87e301af392815af342d8fb3cdadf06da02
jackysee <jackysee@gmail.com>
2021-01-11 22:39:47 +0800

    2019 d24 wip

---
+++ 2019/day24.js
@@ -0,0 +1,15 @@
+//AOC2019 D24
+
+function parse(str) {
+    let map = str.split('\n').map(l => l.split(''));
+}
+
+function data() {
+    return `
+.#..#
+#..##
+##..#
+##.##
+#..##
+    `.trim();
+}
