52af7a9dda03695765ec2c544cbe27051a52673f
jackysee <jackysee@gmail.com>
2021-01-12 07:00:27 +0800

    2019 d24a

--- 2019/day24.js
+++ 2019/day24.js
@@ -20,18 +20,45 @@ function rating(map) {
 
 function part1(str) {
     let map = parse(str);
+    let ratings = new Set();
+    while(true) {
+        let _map = [];
+        for(let i=0; i<5; i++) {
+            _map.push([]);
+            for(let j=0; j<5; j++) {
+                let k = map[i][j];
+                let bugs = [[1,0],[0,1],[-1,0],[0,-1]]
+                    .map(([x,y]) => (map[i+x] || {})[j+y])
+                    .filter(n => n === '#')
+                    .length;
+                if(k === '#' && bugs !== 1) {
+                    k = '.';
+                } else if(k === '.' && (bugs === 1 || bugs === 2)) {
+                    k = '#';
+                }
+                _map[i][j] = k;
+            }
+        }
+        let r = rating(_map);
+        if(ratings.has(r)) {
+            return r;
+        }
+        ratings.add(r);
+        map = _map;
+    }
 }
 
-console.log(rating(parse(sample())));
+// console.log(rating(parse(sample())));
+console.log(part1(data()));
 
 function sample() {
     return `
-.....
-.....
-.....
+....#
+#..#.
+#..##
+..#..
 #....
-.#...
-    `.trim();
+`.trim();
 }
 
 function data() {
