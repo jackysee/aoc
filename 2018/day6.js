cb40d15c6b39a8454fb8226f83c149b430f82a95
jackysee <jackysee@gmail.com>
2021-01-13 18:32:12 +0800

    2018 6a

--- 2018/day6.js
+++ 2018/day6.js
@@ -1,6 +1,62 @@
 //AOC2018 D6
 
+function parse(s) {
+    return s.split('\n').map(l => l.split(', ').map(Number));
+}
 
+function part1(s) {
+    let pts = parse(s);
+    let xs = pts.map(p => p[0]);
+    let ys = pts.map(p => p[1]);
+    let x1 = Math.min(...xs);
+    let x2 = Math.max(...xs);
+    let y1 = Math.min(...ys);
+    let y2 = Math.max(...ys);
+    let area = Array(pts.length).fill(0);
+    let edges = new Set();
+    for(let y=y1; y<=y2; y++) {
+        for(let x=x1; x<=x2; x++) {
+            let dist = pts.map((p, i) => {
+                return Math.abs(p[0] - x) + Math.abs(p[1] - y);
+            });
+            let minDist = Math.min(...dist);
+            if(dist.filter(d => d === minDist).length > 1) {
+                continue;
+            }
+            let idx = dist.findIndex(d => d === minDist);
+            area[idx] += 1;
+            if(x == x1 || x === x2 || y === y1 || y === y2) {
+                edges.add(idx);
+            }
+        }
+    }
+
+    let m = 0, idx;
+    Object.entries(area)
+        .filter(e => !edges.has(e[0]))
+        .forEach(([i, v]) => {
+            if(v > m) {
+                m = v;
+                idx = i;
+            }
+        });
+    return m;
+}
+
+// console.log(part1(sample()));
+console.log(part1(data()));
+
+function sample() {
+    return `
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9
+    `.trim();
+
+}
 
 function data() {
     return `
