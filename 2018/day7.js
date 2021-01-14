7d05f40698d3ff5cd5179a54784d03896b0d8dcf
jackysee <jackysee@gmail.com>
2021-01-14 18:08:45 +0800

    2018 d7

--- 2018/day7.js
+++ 2018/day7.js
@@ -4,25 +4,87 @@ function parse(s) {
     let arr = s.split('\n').map(l => {
         return l.match(/ ([A-Z])/g).map(s => s.trim());
     });
+    let all =  new Set([...arr.flat()]);
     let map = {};
-    let depends = new Set();
-    arr.forEach(([from, to]) => {
-        map[from] = map[from] || [];
-        map[from].push(to);
-        depends.add(to);
+    [...all].forEach(n => {
+        map[n] = arr.filter(([n1, n2]) => n2 === n).map(a => a[0]);
     });
-    for(let m in map) {
-        console.log(map[m]);
-        map[m] = map[m].sort();
-    }
-    let root = [...new Set(arr.flat())]
-        .filter(s => !depends.has(s))
-        .sort();
-    map.root = root;
     return map;
 }
 
-console.log(parse(data()));
+function print(map) {
+    let list = Object.entries(map);
+    let ready = new Set();
+    while(true) {
+        let _list = list
+            .filter(([n, req]) => !ready.has(n) && req.every(_n => ready.has(_n)))
+            .map(a => a[0]).sort();
+        if(!_list.length) {
+            break;
+        }
+        ready.add(_list[0]);
+    }
+    return [...ready].join('');
+}
+
+function getTime(n, base) {
+    return base + n.charCodeAt(0) - 64;
+}
+
+function work(map, workers = 5, base = 60) {
+    let list = Object.entries(map);
+    let working = [];
+    let ready = new Set();
+    let t = 0;
+    while(true) {
+        working = working.map(w => {
+            let t = w.time - 1;
+            if(t === 0) ready.add(w.step);
+            return { step:w.step, time: t };
+        }).filter(w => w.time !== 0);
+        
+        let _list = list
+            .filter(([n, req]) => !ready.has(n) && req.every(_n => ready.has(_n)))
+            .map(a => a[0]).sort();
+
+        if(!_list.length) {
+            break;
+        }
+        
+        _list = _list.filter(n => !working.find(w => w.step === n));
+        let i = 0;
+        while(working.length < 5 && _list.length > 0) {
+            let n = _list.shift();
+            working.push({ step:n, time: getTime(n, base) })
+        }
+        t++;
+    }
+    return [[...ready].join(''), t]; 
+}
+
+
+// console.log(parse(data()));
+// console.log(print(parse(sample())));
+// console.log(work(parse(sample()), 2, 0));
+
+console.log(print(parse(data())));
+console.log(work(parse(data()))); //1133
+
+
+
+
+
+function sample() {
+    return `
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+    `.trim();
+}
 
 function data() {
     return `
