9ce7da95163ed67a275270c9bda49cd66de5e09d
jackysee <jackysee@gmail.com>
2021-12-01 15:14:01 +0800

    2021

--- 2018/day1.js
+++ 2018/day1.js
@@ -4,17 +4,19 @@ function parse(s) {
     return s.split('\n').map(Number);
 }
 
-console.log(parse(data()).reduce((a,c) => a + c, 0));
+console.log(parse(data()).reduce((a, c) => a + c, 0));
 
 function part2(str) {
     let nums = parse(str);
     let s = new Set();
     let freq = 0;
     let i = 0;
-    while(true) {
-        let n = nums[i%nums.length];
+    while (true) {
+        let n = nums[i % nums.length];
         freq += n;
-        if(s.has(freq)) { return freq; }
+        if (s.has(freq)) {
+            return freq;
+        }
         s.add(freq);
         i++;
     }
---
+++ 2021/template.ts
@@ -0,0 +1,10 @@
+let arr: Array<number> = data()
+    .trim()
+    .split('\n')
+    .map((s) => parseInt(s, 10));
+
+function data() {
+    return `
+
+`.trim();
+}
