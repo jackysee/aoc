773ac60c95e450fa0c7e8575c6b7bf0ef2b1cd02
jackysee <jackysee@gmail.com>
2021-12-02 10:12:55 +0800

    config deno lsp

--- get_puzzle.sh
+++ get_puzzle.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo 'usage: ./get_puzzle.sh 2021 1'
+    exit 1
+fi
+
 data=$(curl -s -b cookiefile https://adventofcode.com/$1/day/$2/input)
 
 tee $1/day$2_input.ts <<EOF
@@ -6,3 +12,5 @@ export default function() {
     return \`${data}\`;
 }
 EOF
+
+echo "File written to $1/day$2_input.ts"
