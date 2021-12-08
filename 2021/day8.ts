c5f5bdd3d62018ec9cdec4f0ec53d530d2c20e67
jackysee <jackysee@gmail.com>
2021-12-08 23:25:36 +0800

    refactor

--- 2021/day8.ts
+++ 2021/day8.ts
@@ -6,24 +6,21 @@ interface Entry {
     patterns: string[];
     outputs: string[];
 }
+
 function toEntry(s: string) {
-    let [patterns, outputs] = s.split(/\s*\|\s*/).map((s) => {
-        let r = s.split(/\s+/);
-        return r;
-    });
+    let [patterns, outputs] = s.split(/\s*\|\s*/).map((s) => s.split(/\s+/));
     return { patterns, outputs };
 }
 
 let arr: Entry[] = data().trim().split('\n').map(toEntry);
 
-const segmentLength = [2, 4, 3, 7]; //correspond to 1,4,7,8
 console.log(
     'Part 1',
-    arr.reduce((a, e) => {
-        return (
-            a + e.outputs.filter((s) => segmentLength.includes(s.length)).length
-        );
-    }, 0)
+    arr.reduce(
+        (a, e) =>
+            a + e.outputs.filter((s) => [2, 4, 3, 7].includes(s.length)).length,
+        0
+    )
 );
 
 const diff = (p1: string[], p2: string[]) => {
@@ -37,13 +34,10 @@ const diff = (p1: string[], p2: string[]) => {
     return r;
 };
 
-const remove = (L: string[][], D: string[]) => {
-    return L.filter((p) => p.join('') !== D.join(''));
-};
+const remove = (L: string[][], D: string[]) =>
+    L.filter((p) => p.join('') !== D.join(''));
 
-const sortStr = (s: string) => {
-    return s.split('').sort().join('');
-};
+const sortStr = (s: string) => s.split('').sort().join('');
 
 const getPatternNumberMap = (patterns: string[]) => {
     let L = patterns.reduce((a: { [key: number]: string[][] }, e) => {
@@ -63,42 +57,37 @@ const getPatternNumberMap = (patterns: string[]) => {
         })
     );
 
-    let D1 = L[2][0].sort();
-    let D4 = L[4][0].sort();
-
-    //segment
+    //segment: T, L1, R1, M, L2, R2, B
     let S: { [key: string]: string } = {};
 
     /* L6 not contains D1 is 6 */
+    let D1 = L[2][0].sort();
     let D6 = L[6].find((p) => !D1.every((c) => p.includes(c))) || [];
     L[6] = remove(L[6], D6);
 
     /* D1 not in D6 is R1 */
-    S.R1 = L[2][0].filter((c) => !D6.includes(c))[0];
+    S.R1 = D1.filter((c) => !D6.includes(c))[0];
 
     /* D1 diff [R1] = R2 */
-    S.R2 = diff(L[2][0], [S.R1])[0];
+    S.R2 = diff(D1, [S.R1])[0];
 
     /* L5 no R2 is 2 */
     let D2 = L[5].find((p) => !p.includes(S.R2)) || [];
     L[5] = remove(L[5], D2);
 
     /* D4 diff [...D2, R2] = L1 */
+    let D4 = L[4][0].sort();
     S.L1 = diff(D4, [...D2, S.R2])[0];
 
-    /* L5 filter D2 has L1 = 5 */
+    /* L5 filter D2 has L1 = 5, other 3 */
     let D5 = L[5].filter((p) => p.includes(S.L1))[0];
-
-    /* L5 only one left is 3 */
     let D3 = remove(L[5], D5)[0];
 
     /* D2 - D3 = L2 */
     S.L2 = D2.filter((c) => !D3.includes(c))[0];
 
-    /* L6 no L2 = 9 */
+    /* L6 no L2 = 9, remain 0 */
     let D9 = L[6].filter((p) => !p.includes(S.L2))[0];
-
-    /* the remaining one is 0 */
     let D0 = remove(L[6], D9)[0];
 
     M[D6.sort().join('')] = 6;
@@ -116,10 +105,6 @@ const getOutput = (e: Entry) => {
     let s = outputs.map((s) => M[s.split('').sort().join('')]).join('');
     return parseInt(s, 10);
 };
-
-// let sample = `acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf`;
-// let e = toEntry(sample);
-// console.log(getOutput(toEntry(sample)));
 
 console.log(
     'Part 2',
