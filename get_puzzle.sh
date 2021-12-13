d7452d56026f39c9686838d71366ec6677a816e2
jackysee <jackysee@gmail.com>
2021-12-13 23:30:53 +0800

    get script

--- get_puzzle.sh
+++ get_puzzle.sh
@@ -8,9 +8,13 @@ fi
 data=$(curl -s -b cookiefile https://adventofcode.com/$1/day/$2/input)
 
 tee $1/day$2_input.ts <<EOF
-export default function() { 
-    return \`${data}\`;
-}
+export default () => \`${data}\`;
+EOF
+
+tee $1/day$2_sample.ts <<EOF
+export default () => \`
+
+\`.trim();
 EOF
 
 echo "Data file written to $1/day$2_input.ts"
