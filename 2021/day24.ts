f0e902db457c1b55a416e3c67c0dc47a3668bbfa
jackysee <jackysee@gmail.com>
2021-12-28 09:21:44 +0800

    refactoring

--- 2021/day24.ts
+++ 2021/day24.ts
@@ -1,94 +1,41 @@
-import data from './day24_input.ts';
-// import data from './day24_sample.ts';
-
-interface Instruction {
-    op: string;
-    a: string;
-    b: string | number | undefined;
-}
-
-let ins: Instruction[] = data()
-    .split('\n')
-    .map((s) => {
-        let [op, a, b] = s.split(' ');
-        return {
-            op,
-            a,
-            b: /\d+/.test(b) ? Number(b) : b
-        };
-    });
-
-let n = 11111111111111;
-let max = n;
-while (n <= 99999999999999) {
-    const val = { w: 0, x: 0, y: 0, z: 0 };
-    type k = keyof typeof val;
-    let ns = (n + '').split('').map(Number);
-    if (ns.some((n) => n === 0)) {
-        n++;
-        continue;
-    }
-    ins.forEach((i) => {
-        let op = i.op;
-        let a = i.a as k;
-        let b = (typeof i.b === 'string' ? (val[i.b as k] as number) : i.b)!;
-        if (op === 'inp') {
-            val[a] = ns.shift()!;
-        }
-        if (op === 'add') {
-            val[a] = val[a] + b;
-        }
-        if (op === 'mul') {
-            val[a] = val[a] * b;
-        }
-        if (op === 'div') {
-            val[a] = Math.floor(val[a] / b);
-        }
-        if (op === 'mod') {
-            val[a] = val[a] % b;
-        }
-        if (op === 'eq') {
-            val[a] = val[a] === b ? 1 : 0;
-        }
-    });
-    if (val.z === 0) {
-        max = Math.max(n, max);
-        console.log('update max', max);
-    }
-    console.log(n);
-    n++;
-}
-
-console.log('Part 1', max);
+//import data from './day24_input.ts';
 
 /*
- *
-11111111111111
 
-w = int(input())
-x = int((z % 26) + b != w)
-z //= a
+w = input_digit
+x = !!((z % 26) + b != w)
+z = Math.floor(z / a)
 z *= 25*x+1
 z += (w+c)*x
 
 
 when a == 1, z = z * 26 + w + c (push w+c to base 26)
-when a == 26, 
+when a == 26, x needs to be 0, so z = old_z - b
 
-1 10 13
-1 13 10
-1 13 3
-26 -11 1
-1 11 9
-26 -4 3
-1 12 5
-1 12 1
-1 15 0
-26 -2 13
-26 -5 7
-26 -11 15
-26 -13 12
-26 -10 8
+A: 1 10 13 ==> push A + 13
+B: 1 13 10 ==> push B + 10
+C: 1 13 3  ==> push C + 3
+D: 26 -11 1 ==> pop D = C + 3 - 11
+E: 1 11 9 ==> push E + 9
+F: 26 -4 3 ==> pop F = E + 9 - 4
+G: 1 12 5 ==> push G + 5
+H: 1 12 1 ==> push H + 1
+I: 1 15 0 ==> push I
+J: 26 -2 13  ==> pop J = I - 2
+K: 26 -5 7 ==>  pop K = H + 1 - 5
+L: 26 -11 15 ==> pop L = G + 5 - 11
+M: 26 -13 12 ==> pop M = B + 10 - 13
+N: 26 -10 8 ==> pop N = A + 13 - 10
  
+C + 3 - 11 = D
+E + 9 - 4 = F
+I - 2 = J
+H + 1 - 5 = K
+G + 5 - 11 = L
+B + 10 - 13 = M 
+A + 13 - 10 = N
+
+Part 1: 69914999975369
+Part 2: 14911675311114
 
 */
