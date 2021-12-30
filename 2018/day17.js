a5ed770e842d86f79d883f7bd7f788bcbad0c3ce
jackysee <jackysee@gmail.com>
2021-12-30 10:42:00 +0800

    2018 day 17

--- 2018/day17.js
+++ 2018/day17.js
@@ -1,7 +1,7 @@
 // const data = require('./day17_sample.js');
 const data = require('./day17_input.js');
 
-let M = { '500,0': '+' };
+let M = {};
 data()
     .split('\n')
     .forEach((s) => {
@@ -18,19 +18,98 @@ data()
         }
     });
 
+const toXY = (p) => p.split(',').map(Number);
+
+const getRange = (M) => {
+    let points = Object.keys(M).map(toXY);
+    let xs = points.map((p) => p[0]);
+    let ys = points.map((p) => p[1]);
+    let minX = Math.min(...xs);
+    let maxX = Math.max(...xs);
+    let minY = Math.min(...ys);
+    let maxY = Math.max(...ys);
+    return { minX, maxX, minY, maxY };
+};
+
+let R = getRange(M);
+
 const printMap = (M) => {
-    let points = Object.keys(M).map((s) => s.split(',').map(Number));
-    let minX = Math.min(...points.map((p) => p[0]));
-    let maxX = Math.max(...points.map((p) => p[0]));
-    let minY = Math.min(...points.map((p) => p[1]));
-    let maxY = Math.max(...points.map((p) => p[1]));
+    let r = getRange(M);
     let str = '';
-    for (let y = minY; y <= maxY; y++) {
-        for (let x = minX; x <= maxX; x++) {
-            str += M[[x, y]] || '.';
+    for (let y = r.minY; y <= r.maxY; y++) {
+        for (let x = r.minX; x <= r.maxX; x++) {
+            str += M[[x, y]] || ' ';
         }
         str += '\n';
     }
-    console.log(str);
+    return str;
 };
-printMap(M);
+
+M['500,0'] = '+';
+let queue = ['500,0']; //source
+
+const fillRow = (x, y) => {
+    let row = [];
+    let [_x, _y] = [x, y];
+    while (true) {
+        if (_x > R.maxX) break;
+        if (M[[_x, _y] + ''] === '#') break;
+        row.push([_x, _y]);
+        _x++;
+    }
+    [_x, _y] = [x, y];
+    while (true) {
+        if (_x < R.minX) break;
+        if (M[[_x, _y] + ''] === '#') break;
+        row.push([_x, _y]);
+        _x--;
+    }
+    let sources = [];
+    if (row.every((p) => M[p] === '|')) {
+        row.forEach(([x, y]) => {
+            M[[x, y] + ''] = '~';
+            if (M[[x, y - 1] + ''] === '|') {
+                sources.push([x, y - 1] + '');
+            }
+        });
+    }
+    return sources;
+};
+
+while (queue.length) {
+    let pt = queue.shift();
+    let [x, y] = toXY(pt);
+    let down = [x, y + 1] + '';
+    let left = [x - 1, y] + '';
+    let right = [x + 1, y] + '';
+    if (M[pt] === '+' || M[pt] === '|') {
+        if (M[down] === undefined && y < R.maxY) {
+            M[down] = '|';
+            queue.push(down);
+        }
+        if (M[down] === '#' || M[down] === '~') {
+            if (M[left] === undefined) {
+                M[left] = '|';
+                queue.push(left);
+            }
+            if (M[right] === undefined) {
+                M[right] = '|';
+                queue.push(right);
+            }
+            if ([M[left], M[right]].some((v) => ['#', '|'].includes(v))) {
+                let sources = fillRow(x, y);
+                if (sources) queue.push(...sources);
+            }
+        }
+    }
+}
+
+const count = (M, pattern) => {
+    return Object.entries(M).filter(([k, v]) => {
+        let y = toXY(k)[1];
+        return y >= R.minY && y <= R.maxY && pattern.test(v);
+    }).length;
+};
+
+console.log('Part 1', count(M, /\||~/));
+console.log('Part 2', count(M, /~/));
