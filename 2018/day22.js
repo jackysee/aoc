b5d6f40f5356d15b506649968d4621e65182cbcd
jackysee <jackysee@gmail.com>
2022-01-03 22:48:18 +0800

    18 22 a

--- 2018/day22.js
+++ 2018/day22.js
@@ -1,9 +1,8 @@
-// const depth = 4002;
-// const target = [5,746];
-const depth = 510;
-const target = [10, 10];
+const depth = 4002;
+const target = [5, 746];
+// const depth = 510;
+// const target = [10, 10];
 const n = 20183;
-
 const erosion = {};
 const geo = {};
 const type = {};
@@ -20,20 +19,14 @@ for (let x = 1; x <= target[0]; x++) {
 }
 for (let y = 0; y <= target[1]; y++) {
     for (let x = 0; x <= target[0]; x++) {
-        // (a + b ) mod n = [(a mod n) + (b mod n)] mod n
-        // ab mod n = [(a mod n)(b mod n)] mod n;
         if (geo[[x, y]] === undefined) {
-            // console.log('define', [x, y], geo[[x - 1, y]], geo[[x, y - 1]]);
-            // geo[[x, y]] = geo[[x - 1, y]] * geo[[x, y - 1]];
-            let g = (((geo[[x - 1, y]] % n) * geo[[x, y - 1]]) % n) % n;
-            erosion[[x, y]] = (g + (depth % n)) % n;
-        } else {
-            erosion[[x, y]] = (geo[[x, y]] + depth) % 20183;
+            geo[[x, y]] = erosion[[x - 1, y]] * erosion[[x, y - 1]];
         }
+        erosion[[x, y]] = (geo[[x, y]] + depth) % n;
         let e = erosion[[x, y]] % 3;
-        type[[x, y]] = ['rocky', 'wet', 'narrow'][e];
+        type[[x, y]] = e; //['rocky', 'wet', 'narrow'][e];
         risk += e;
     }
 }
 
-console.log(risk);
+console.log('Part 1', risk);
