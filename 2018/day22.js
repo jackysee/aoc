635ea78fb0b9fc52d3253b3a0c93701bc466d1d2
jackysee <jackysee@gmail.com>
2022-01-04 15:54:59 +0800

    18 22 b

--- 2018/day22.js
+++ 2018/day22.js
@@ -2,31 +2,67 @@ const depth = 4002;
 const target = [5, 746];
 // const depth = 510;
 // const target = [10, 10];
+
 const n = 20183;
-const erosion = {};
-const geo = {};
-const type = {};
-geo[[0, 0]] = 0;
-geo[target] = 0;
+
+const memoize = (fn) => {
+    const cache = {};
+    return (x, y) => {
+        if (cache[[x, y]]) return cache[[x, y]];
+        return (cache[[x, y]] = fn(x, y));
+    };
+};
+
+const geo = memoize((x, y) => {
+    if (x == 0 && y === 0) return 0;
+    if (x == target[0] && y === target[1]) return 0;
+    if (x === 0) return y * 48271;
+    if (y === 0) return x * 16807;
+    return erosion(x - 1, y) * erosion(x, y - 1);
+});
+
+const erosion = memoize((x, y) => (geo(x, y) + depth) % n);
+
+const getType = memoize((x, y) => erosion(x, y) % 3);
 
 let risk = 0;
-
-for (let y = 1; y <= target[1]; y++) {
-    geo[[0, y]] = y * 48271;
-}
-for (let x = 1; x <= target[0]; x++) {
-    geo[[x, 0]] = x * 16807;
-}
 for (let y = 0; y <= target[1]; y++) {
     for (let x = 0; x <= target[0]; x++) {
-        if (geo[[x, y]] === undefined) {
-            geo[[x, y]] = erosion[[x - 1, y]] * erosion[[x, y - 1]];
-        }
-        erosion[[x, y]] = (geo[[x, y]] + depth) % n;
-        let e = erosion[[x, y]] % 3;
-        type[[x, y]] = e; //['rocky', 'wet', 'narrow'][e];
-        risk += e;
+        risk += getType(x, y);
     }
 }
-
 console.log('Part 1', risk);
+
+const heap = require('./binaryHeap.js');
+let queue = heap((a, b) => a.t < b.t);
+queue.push({ pos: [0, 0], t: 0, tool: 'torch' });
+let tools = ['neither', 'torch', 'gear'];
+const T = {};
+let t = Date.now();
+while (queue.size()) {
+    let pt = queue.pop();
+    let [x, y] = pt.pos;
+    let _t = T[[x, y, pt.tool]];
+    if (_t !== undefined && _t <= pt.t) continue;
+    T[[x, y, pt.tool]] = pt.t;
+
+    if (pt.pos + '' === target + '' && pt.tool === 'torch') {
+        console.log('Part 2', pt.t, `(took ${Date.now() - t}ms)`);
+        break;
+    }
+
+    tools
+        .filter((tool, i) => getType(x, y) !== i && tool !== pt.tool)
+        .forEach((tool) => queue.push({ ...pt, t: pt.t + 7, tool }));
+
+    [
+        [x, y - 1],
+        [x, y + 1],
+        [x - 1, y],
+        [x + 1, y]
+    ].forEach(([x, y]) => {
+        if (x < 0 || y < 0) return;
+        if (pt.tool === tools[getType(x, y)]) return;
+        queue.push({ pos: [x, y], t: pt.t + 1, tool: pt.tool });
+    });
+}
