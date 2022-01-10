dbd48bb24b6e26c6d02866f28ab08ec7b823e752
jackysee <jackysee@gmail.com>
2022-01-10 23:07:39 +0800

    15 16

--- 2015/day16.ts
+++ 2015/day16.ts
@@ -19,4 +19,31 @@ let arr: Sue[] = data()
         return { id, attrs };
     });
 
-console.log(arr);
+const clues = Object.entries({
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1
+});
+
+const matchClues = (s: Sue) =>
+    clues.filter(([k, v]) => s.attrs[k] === v).length;
+
+arr.sort((a, b) => matchClues(b) - matchClues(a));
+console.log('Part 1', arr[0].id);
+
+const matchClues2 = (s: Sue) =>
+    clues.filter(([k, v]) => {
+        if (/cats|trees/.test(k)) return s.attrs[k] > v;
+        if (/pomeranians|goldfish/.test(k)) return s.attrs[k] < v;
+        return s.attrs[k] === v;
+    }).length;
+
+arr.sort((a, b) => matchClues2(b) - matchClues2(a));
+console.log('Part 2', arr[0].id);
