9cc09daec60a6d1f0d1d2b65707b75e5c53511f9
jackysee <jackysee@gmail.com>
2022-01-15 00:10:59 +0800

    15 23

--- 2015/day23.ts
+++ 2015/day23.ts
@@ -1,39 +1,23 @@
 import data from './day23_input.ts';
-// import data from './day23_sample.ts';
 
-interface Instruction {
-    op: string;
-    reg?: string;
-    offset?: number;
-}
-
-let arr: Instruction[] = data()
+let arr: string[][] = data()
     .split('\n')
-    .map((l) => {
-        let op = l.match(/^(\w+)/)![0];
-        let m_reg = l.match(/a|b/);
-        let reg = m_reg ? m_reg[0] : undefined;
-        let m_offset = l.match(/[\+\-]\d+/);
-        let offset = m_offset ? Number(m_offset) : undefined;
-        return { op, reg, offset };
-    });
+    .map((l) => l.match(/[^, ]+/g)!);
 
 function run(r: Record<string, number>) {
     let idx = 0;
     while (arr[idx]) {
-        let { op, reg, offset } = arr[idx];
-        if (op === 'hlf') r[reg!] /= 2;
-        if (op === 'tpl') r[reg!] *= 3;
-        if (op === 'inc') r[reg!] += 1;
+        let [op, a, b] = arr[idx];
+        if (op === 'hlf') r[a] /= 2;
+        if (op === 'tpl') r[a] *= 3;
+        if (op === 'inc') r[a] += 1;
         if (!/^j/.test(op)) idx += 1;
-        if (op === 'jmp') idx += offset!;
-        if (op === 'jie') idx += r[reg!] % 2 === 0 ? offset! : 1;
-        if (op === 'jio') idx += r[reg!] % 2 === 1 ? offset! : 1;
-        console.log(op, reg, offset || '', r, idx);
+        if (op === 'jmp') idx += +a;
+        if (op === 'jie') idx += r[a] % 2 === 0 ? +b : 1;
+        if (op === 'jio') idx += r[a] === 1 ? +b : 1;
     }
     return r;
 }
 
-console.log(arr);
-
-console.log(run({ a: 0, b: 0 }));
+console.log('Part 1', run({ a: 0, b: 0 }).b);
+console.log('Part 2', run({ a: 1, b: 0 }).b);
