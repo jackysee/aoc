d6c3272f9f3fecfb5f61b18fec22315f083a88b9
jackysee <jackysee@gmail.com>
2022-01-14 16:55:22 +0800

    15 22b

--- 2015/day22.ts
+++ 2015/day22.ts
@@ -5,7 +5,6 @@ interface Player {
     mana: number;
     manaUsed: number;
     activeSpells: Record<string, number>;
-    history: string[];
 }
 interface Boss {
     hp: number;
@@ -13,13 +12,14 @@ interface Boss {
 }
 const spells = [
     {
-        id: 'missles',
+        id: 'missile',
         cost: 53,
         cast: (player: Player, boss: Boss) => {
             player.mana -= 53;
             player.manaUsed += 53;
             boss.hp -= 4;
-        }
+        },
+        effect: () => {}
     },
     {
         id: 'drain',
@@ -29,19 +29,22 @@ const spells = [
             player.manaUsed += 73;
             player.hp += 2;
             boss.hp -= 2;
-        }
+        },
+        effect: () => {}
     },
     {
         id: 'shield',
         cost: 113,
         cast: (player: Player, boss?: Boss) => {
-            if (!player.activeSpells.shield) {
+            if (player.activeSpells.shield === undefined) {
                 player.mana -= 113;
                 player.manaUsed += 113;
                 player.armor += 7;
                 player.activeSpells.shield = 6;
                 return;
             }
+        },
+        effect: (player: Player, boss: Boss) => {
             player.activeSpells.shield -= 1;
             if (player.activeSpells.shield === 0) {
                 player.armor -= 7;
@@ -53,12 +56,14 @@ const spells = [
         id: 'poison',
         cost: 173,
         cast: (player: Player, boss: Boss) => {
-            if (!player.activeSpells.poison) {
+            if (player.activeSpells.poison === undefined) {
                 player.mana -= 173;
                 player.manaUsed += 173;
                 player.activeSpells.poison = 6;
                 return;
             }
+        },
+        effect: (player: Player, boss: Boss) => {
             boss.hp -= 3;
             player.activeSpells.poison -= 1;
             if (player.activeSpells.poison === 0) {
@@ -70,12 +75,14 @@ const spells = [
         id: 'recharge',
         cost: 229,
         cast: (player: Player, boss?: Boss) => {
-            if (!player.activeSpells.recharge) {
+            if (player.activeSpells.recharge === undefined) {
                 player.mana -= 229;
                 player.manaUsed += 229;
                 player.activeSpells.recharge = 5;
                 return;
             }
+        },
+        effect: (player: Player, boss?: Boss) => {
             player.mana += 101;
             player.activeSpells.recharge -= 1;
             if (player.activeSpells.recharge === 0) {
@@ -92,84 +99,67 @@ const player = {
     armor: 0,
     mana: 500,
     manaUsed: 0,
-    activeSpells: {},
-    history: [] as string[]
+    activeSpells: {}
 };
 
-function play(_player: Player, _boss: Boss, spellId: string): [Player, Boss] {
+function applyActiveSpells(player: Player, boss: Boss) {
+    let keys = Object.keys(player.activeSpells);
+    for (let i = 0; i < keys.length; i++) {
+        spells.find((_s) => _s.id == keys[i])!.effect(player, boss);
+        if (player.hp <= 0 || boss.hp <= 0) return;
+    }
+}
+
+function play(
+    _player: Player,
+    _boss: Boss,
+    spellId: string,
+    hard: boolean
+): [Player, Boss] {
     let player = {
         ..._player,
-        activeSpells: { ..._player.activeSpells },
-        history: [..._player.history, spellId]
+        activeSpells: { ..._player.activeSpells }
     };
     let boss = { ..._boss };
     let spell = spells.find((s) => s.id === spellId)!;
 
     //player turn
-    Object.keys(player.activeSpells).forEach((id) => {
-        spells.find((_s) => _s.id == id)!.cast(player, boss);
-    });
+    if (hard) player.hp -= 1;
+    if (player.hp <= 0) return [player, boss];
+    applyActiveSpells(player, boss);
     spell.cast(player, boss);
+    if (boss.hp <= 0) return [player, boss];
 
     //boss turn
-    Object.keys(player.activeSpells).forEach((id) => {
-        spells.find((_s) => _s.id == id)!.cast(player, boss);
-    });
+    applyActiveSpells(player, boss);
+    if (boss.hp <= 0) return [player, boss];
     player.hp -= Math.max(1, boss.damage - player.armor);
     return [player, boss];
 }
 
-function findLowestMana(player: Player, boss: Boss) {
+function findLowestMana(player: Player, boss: Boss, hard: boolean = false) {
     let minManaUsed = Infinity;
     let queue = [{ boss, player }];
     while (queue.length) {
         let p = queue.pop()!;
+        if (p.player.hp <= 0) continue;
         if (p.boss.hp <= 0) {
             if (p.player.manaUsed < minManaUsed) {
                 minManaUsed = p.player.manaUsed;
             }
             continue;
         }
-        if (p.player.hp <= 0 || p.player.manaUsed >= minManaUsed) continue;
-        spells
-            .filter(
-                (s) =>
-                    ((p.player as Player).activeSpells[s.id] || 0) === 0 &&
-                    p.player.mana >= s.cost
-            )
-            .forEach((s) => {
-                let [player, boss] = play(p.player, p.boss, s.id);
-                queue.push({ player, boss });
-            });
-        // queue.sort((a, b) => b.player.manaUsed - a.player.manaUsed);
+        if (p.player.manaUsed >= minManaUsed) continue;
+        spells.forEach((s) => {
+            if ((p.player.activeSpells[s.id] || 0) > 1) return;
+            if (p.player.mana < s.cost) return;
+            let [player, boss] = play(p.player, p.boss, s.id, hard);
+            if (player.hp <= 0) return;
+            queue.push({ player, boss });
+        });
     }
     return minManaUsed;
 }
 
 console.log('Part 1', findLowestMana(player, boss));
-
-// let bb: Boss = { hp: 14, damage: 8 };
-// let pp: Player = {
-//     hp: 10,
-//     damage: 0,
-//     armor: 0,
-//     mana: 250,
-//     manaUsed: 0,
-//     activeSpells: {},
-//     history: [] as string[]
-// };
-
-// [pp, bb] = play(pp, bb, 'recharge');
-// console.log(pp, bb);
-
-// [pp, bb] = play(pp, bb, 'shield');
-// console.log(pp, bb);
-
-// [pp, bb] = play(pp, bb, 'drain');
-// console.log(pp, bb);
-
-// [pp, bb] = play(pp, bb, 'poison');
-// console.log(pp, bb);
-
-// [pp, bb] = play(pp, bb, 'missles');
-// console.log(pp, bb);
+console.log('Part 2', findLowestMana(player, boss, true));
