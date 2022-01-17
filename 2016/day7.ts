fb6a7d5e7c754c683fc9059767fddd6b45d4ee62
jackysee <jackysee@gmail.com>
2022-01-17 18:27:47 +0800

    refactoring

--- 2016/day7.ts
+++ 2016/day7.ts
@@ -25,8 +25,6 @@ console.log(
 const matchOverlap = (input: string, re: RegExp) => {
     var r = [];
     var m;
-    if (!re.global)
-        re = new RegExp(re.source, (re + '').split('/').pop() + 'g');
     while ((m = re.exec(input))) {
         re.lastIndex -= m[0].length - 1;
         r.push(m[0]);
