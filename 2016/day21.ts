ba4524927ed7c765563215db12bac475d15bf6e3
jackysee <jackysee@gmail.com>
2022-01-21 23:17:16 +0800

    16 21b

--- 2016/day21.ts
+++ 2016/day21.ts
@@ -7,54 +7,59 @@ function process(s: string, reverse: boolean = false) {
     let _arr = [...arr];
     if (reverse) _arr = _arr.reverse();
     _arr.forEach((l) => {
+        let [x, y] = (l.match(/\d+/g) || []).map(Number);
+        let [a, b] = l.match(/(?<=letter )\w+/g) || [];
         if (l.startsWith('swap position')) {
-            let [x, y] = l.match(/\d+/g)!.map(Number);
             if (reverse) [x, y] = [y, x];
-            let [a, b] = [pwd[x], pwd[y]];
-            pwd[x] = b;
-            pwd[y] = a;
+            [pwd[y], pwd[x]] = [pwd[x], pwd[y]];
         }
         if (l.startsWith('swap letter')) {
-            let [a, b] = l.match(/(?<=letter )\w+/g)!;
             pwd = pwd.map((c) => (c === a ? b : c === b ? a : c));
         }
         if (l.startsWith('reverse positions')) {
-            let [x, y] = l.match(/\d+/g)!.map(Number);
             pwd = [
                 ...pwd.slice(0, x),
                 ...pwd.slice(x, y + 1).reverse(),
                 ...pwd.slice(y + 1)
             ];
         }
-        const rotateLeft = (x: number) => [...pwd.slice(x), ...pwd.slice(0, x)];
-        const rotateRight = (x: number) => [
+        const rotateLeft = (x: number, pwd: string[]) => [
+            ...pwd.slice(x),
+            ...pwd.slice(0, x)
+        ];
+        const rotateRight = (x: number, pwd: string[]) => [
             ...pwd.slice(-x),
             ...pwd.slice(0, -x)
         ];
         if (l.startsWith('rotate left')) {
-            let [x] = l.match(/\d+/g)!.map(Number);
             x = x % pwd.length;
-            if (reverse) pwd = rotateRight(x);
-            else pwd = rotateLeft(x);
+            pwd = reverse ? rotateRight(x, pwd) : rotateLeft(x, pwd);
         }
         if (l.startsWith('rotate right')) {
-            let [x] = l.match(/\d+/g)!.map(Number);
             x = x % pwd.length;
-            if (reverse) pwd = rotateLeft(x);
-            else pwd = rotateRight(x);
+            pwd = reverse ? rotateLeft(x, pwd) : rotateRight(x, pwd);
         }
         if (l.startsWith('move position')) {
-            let [x, y] = l.match(/\d+/g)!.map(Number);
             if (reverse) [x, y] = [y, x];
             let [c] = pwd.splice(x, 1);
             pwd.splice(y, 0, c);
         }
-        if (l.startsWith('rotate based on')) {
-            let [a] = l.match(/(?<=letter )\w+/g)!;
+        const rotateBased = (a: string, pwd: string[]) => {
             let idx = pwd.findIndex((c) => c === a);
             let r = (1 + idx + (idx >= 4 ? 1 : 0)) % pwd.length;
-            if (reverse) pwd = rotateLeft(r);
-            else pwd = rotateRight(r);
+            return rotateRight(r, pwd);
+        };
+        if (l.startsWith('rotate based on')) {
+            let [a] = l.match(/(?<=letter )\w+/g)!;
+            if (reverse) {
+                let old = [...pwd];
+                while (true) {
+                    pwd = rotateLeft(1, pwd);
+                    if (rotateBased(a, pwd).join('') === old.join('')) break;
+                }
+            } else {
+                pwd = rotateBased(a, pwd);
+            }
         }
     });
     return pwd.join('');
