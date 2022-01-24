63a3d4193e6cb16cfa3025f73e8cae4ac5cac039
jackysee <jackysee@gmail.com>
2022-01-25 06:59:52 +0800

    16 wip:

--- 2016/day23.ts
+++ 2016/day23.ts
@@ -48,27 +48,27 @@ console.log('Part 1', run({ a: 7, b: 0, c: 0, d: 0 }).a);
 
  0 cpy a b      b = a               b = 12
  1 dec b        b -= 1              b = 11
- 2 cpy a d      d = a               d = 12
+ 2 cpy a d      d = a               d = 12, 121
  3 cpy 0 a      a = 0               a = 0
  4 cpy b c      c = b               c = 11
- 5 inc a        a += 1              a = 1 .. 11
+ 5 inc a        a += 1              a = 11 x 11 x 11 x 121
  6 dec c        c -= 1              c = 10 .. 0
  7 jnz c -2     c !== 0 , j 5
- 8 dec d        d -= 1              d = 12
+ 8 dec d        d -= 1              d = 11
  9 jnz d -5     d !== 0, j  4
- 10 dec b
- 11 cpy b c
- 12 cpy c d
- 13 dec d
- 14 inc c
- 15 jnz d -2
- 16 tgl c
- 17 cpy -16 c
- 18 jnz 1 c
+ 10 dec b       b -= 1              b = 11
+ 11 cpy b c     c = b               c = 11
+ 12 cpy c d     d = c               d = 11
+ 13 dec d       d -= 1              d = 10 .. 0
+ 14 inc c       c += 1              c = 12 .. 22
+ 15 jnz d -2    d !== 0, j 13
+ 16 tgl c                           tgl 22
+ 17 cpy -16 c   c = -16
+ 18 jnz 1 c     1 !== 9, j 2
  19 cpy 94 c
  20 jnz 82 d
  21 inc a
- 22 inc d
+ 22 inc d       
  23 jnz d -2
  24 inc c
  25 jnz c -5
