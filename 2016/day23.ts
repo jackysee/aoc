c4e0132fe229d383bb07f20877b2262b9e52b3e9
jackysee <jackysee@gmail.com>
2022-01-25 10:36:50 +0800

    16 23b

--- 2016/day23.ts
+++ 2016/day23.ts
@@ -1,21 +1,16 @@
 import data from './day23_input.ts';
 // import data from './day23_sample.ts';
 
-data()
-    .split('\n')
-    .forEach((l, i) => {
-        console.log(i, l);
-    });
-
-function run(reg: Record<string, number>) {
-    const arr = data().split('\n');
+function run(pg: string, reg: Record<string, number>) {
+    const arr = pg.split('\n');
     const getVal = (v: string) => (isNaN(Number(v)) ? reg[v] : Number(v));
     let i = 0;
     while (arr[i]) {
-        let [op, x, y] = arr[i].split(/\s+/);
+        let [op, x, y, z] = arr[i].split(/\s+/);
         if (op === 'cpy' && reg[y] !== undefined) reg[y] = getVal(x);
         if (op === 'inc' && reg[x] !== undefined) reg[x] += 1;
         if (op === 'dec' && reg[x] !== undefined) reg[x] -= 1;
+        if (op === 'mul') reg[z] = getVal(x) * getVal(y);
         if (op === 'tgl') {
             let target = arr[i + getVal(x)];
             if (target) {
@@ -41,38 +36,34 @@ function run(reg: Record<string, number>) {
     return reg;
 }
 
-console.log('Part 1', run({ a: 7, b: 0, c: 0, d: 0 }).a);
+console.log('Part 1', run(data(), { a: 7, b: 0, c: 0, d: 0 }).a);
 
-/*
- *
-
- 0 cpy a b      b = a               b = 12
- 1 dec b        b -= 1              b = 11
- 2 cpy a d      d = a               d = 12, 121
- 3 cpy 0 a      a = 0               a = 0
- 4 cpy b c      c = b               c = 11
- 5 inc a        a += 1              a = 11 x 11 x 11 x 121
- 6 dec c        c -= 1              c = 10 .. 0
- 7 jnz c -2     c !== 0 , j 5
- 8 dec d        d -= 1              d = 11
- 9 jnz d -5     d !== 0, j  4
- 10 dec b       b -= 1              b = 11
- 11 cpy b c     c = b               c = 11
- 12 cpy c d     d = c               d = 11
- 13 dec d       d -= 1              d = 10 .. 0
- 14 inc c       c += 1              c = 12 .. 22
- 15 jnz d -2    d !== 0, j 13
- 16 tgl c                           tgl 22
- 17 cpy -16 c   c = -16
- 18 jnz 1 c     1 !== 9, j 2
- 19 cpy 94 c
- 20 jnz 82 d
- 21 inc a
- 22 inc d       
- 23 jnz d -2
- 24 inc c
- 25 jnz c -5
-
- */
-
-// console.log('Part 2', run({ a: 12, b: 0, c: 0, d: 0 }).a);
+let pg2 = `
+cpy a b
+dec b
+cpy a d
+cpy 0 a
+mul b d a
+cpy 0 c
+cpy 0 c
+cpy 0 c
+cpy 0 c
+cpy 0 d
+dec b
+cpy b c
+cpy c d
+dec d
+inc c
+jnz d -2
+tgl c
+cpy -16 c
+jnz 1 c
+cpy 94 c
+jnz 82 d
+inc a
+inc d
+jnz d -2
+inc c
+jnz c -5
+`.trim();
+console.log('Part 2', run(pg2, { a: 12, b: 0, c: 0, d: 0 }).a);
