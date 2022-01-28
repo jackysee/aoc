5206a9be33b1c86b6af3bfa85166478df512517f
jackysee <jackysee@gmail.com>
2022-01-29 06:42:38 +0800

    17 19a

---
+++ 2017/day19.ts
@@ -0,0 +1,56 @@
+import data from './day19_input.ts';
+// import data from './day19_sample.ts';
+
+let M: Record<string, string> = {};
+data()
+    .split('\n')
+    .forEach((l, y) => {
+        l.split('').forEach((c, x) => {
+            if (c !== ' ') M[[x, y] + ''] = c;
+        });
+    });
+
+const walk = (dir: string, pos: string) => {
+    let [x, y] = pos.split(',').map(Number);
+    if (dir === 'S') return [x, y + 1] + '';
+    if (dir === 'W') return [x - 1, y] + '';
+    if (dir === 'N') return [x, y - 1] + '';
+    if (dir === 'E') return [x + 1, y] + '';
+    return [x, y] + '';
+};
+const turnLeft = (dir: string) => ({ N: 'W', E: 'N', S: 'E', W: 'S' }[dir]);
+const turnRight = (dir: string) => ({ N: 'E', E: 'S', S: 'W', W: 'N' }[dir]);
+
+let current = '1,0';
+let dir = 'S';
+let result: string[] = [];
+while (true) {
+    // console.log(
+    //     current,
+    //     dir,
+    //     M[current],
+    //     [dir, turnLeft(dir), turnRight(dir)].map((d) => M[walk(d!, current)])
+    // );
+    let next: string = '';
+    if (/[\|\-A-Z]/.test(M[current])) {
+        next = walk(dir, current);
+    }
+    if (M[current] === '+') {
+        let p = walk(turnLeft(dir)!, current);
+        if (/[\|\-A-Z]/.test(M[p])) {
+            dir = turnLeft(dir)!;
+            next = p;
+        } else {
+            let p = walk(turnRight(dir)!, current);
+            if (/[\|\-A-Z]/.test(M[p])) {
+                dir = turnRight(dir)!;
+                next = p;
+            }
+        }
+    }
+    if (current === next) break;
+    current = next;
+    if (/[A-Z]/.test(M[current])) result.push(M[current]);
+}
+
+console.log('Part 1', result.join(''));
