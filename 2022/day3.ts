c2fe78fb0581b3a473ca4d839d29961609c60cb2
jackysee <jackysee@gmail.com>
2022-12-04 07:43:51 +0800

    refactoring

--- 2022/day3.ts
+++ 2022/day3.ts
@@ -2,17 +2,14 @@ import data from './day3_input.ts';
 // import data from './day3_sample.ts';
 
 let arr: string[] = data().split('\n');
-
-const val = (c: string | undefined) => {
-    if (c === undefined) return 0;
-    return c.charCodeAt(0) - (c === c.toUpperCase() ? 38 : 96);
-};
+const L = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const val = (c: string | undefined) => return (c === undefined) 0 : L.indexOf(c) + 1;
+const contains = (str: string, c: string) => str.indexOf(c) !== -1;
 const find = (arr: string[]) => {
     return arr[0]
         .split('')
         .find((c) => arr.slice(1).every((l) => contains(l, c)));
 };
-const contains = (str: string, c: string) => str.indexOf(c) !== -1;
 const partA = arr
     .map((l) => {
         const left = l.slice(0, l.length / 2);
@@ -20,7 +17,6 @@ const partA = arr
         return val(find([left, right]));
     })
     .reduce((a, c) => a + c, 0);
-
 console.log(partA);
 
 const calc = ([x, y, z, ...rest]: string[]): number => {
