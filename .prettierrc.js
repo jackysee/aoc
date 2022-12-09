6ece8cf813a7a3873bab36da50887d98096f3e79
jackysee <jackysee@gmail.com>
2022-12-09 09:51:45 +0800

    refactoring, prettier tab space 2

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     semi: true,
     singleQuote: true,
-    tabWidth: 2,
+    tabWidth: 4,
     endOfLine: 'lf',
     trailingComma: 'none'
 };
--- 2022/day8.ts
+++ 2022/day8.ts
@@ -2,40 +2,49 @@ import data from './day8_input.ts';
 // import data from './day8_sample.ts';
 
 const arr: number[][] = data()
-  .split('\n')
-  .map((l) => l.split('').map(Number));
+    .split('\n')
+    .map((l) => l.split('').map(Number));
 const W = arr[0].length;
 const H = arr.length;
 
-const getViews = (x: number, y: number) => {
-  const result: number[][] = [[], [], [], []];
-  for (let i = x - 1; i >= 0; i--) result[0].push(arr[y][i]);
-  for (let i = x + 1; i <= W - 1; i++) result[1].push(arr[y][i]);
-  for (let j = y - 1; j >= 0; j--) result[2].push(arr[j][x]);
-  for (let j = y + 1; j <= H - 1; j++) result[3].push(arr[j][x]);
-  return result;
-};
+const DIR = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0]
+];
+const getViews = (x: number, y: number) =>
+    DIR.map(([dx, dy]) => {
+        const a = [];
+        let [_x, _y] = [x, y];
+        while (true) {
+            [_x, _y] = [_x + dx, _y + dy];
+            if (_x < 0 || _x >= W || _y < 0 || _y >= H) break;
+            a.push(arr[_y][_x]);
+        }
+        return a;
+    });
 
 const isVisible = (x: number, y: number) =>
-  getViews(x, y).some((s) => s.every((n) => n < arr[y][x]) || s.length === 0);
+    getViews(x, y).some((s) => s.every((n) => n < arr[y][x]) || s.length === 0);
 
 const dist = (v: number, arr: number[]) => {
-  const i = arr.findIndex((n) => n >= v);
-  return i === -1 ? arr.length : i + 1;
+    const i = arr.findIndex((n) => n >= v);
+    return i === -1 ? arr.length : i + 1;
 };
 
 const getScore = (x: number, y: number) =>
-  getViews(x, y)
-    .map((a) => dist(arr[y][x], a))
-    .reduce((a, c) => a * c, 1);
+    getViews(x, y)
+        .map((a) => dist(arr[y][x], a))
+        .reduce((a, c) => a * c, 1);
 
 let count = 0;
 let score = 0;
 for (let j = 0; j < H; j++) {
-  for (let i = 0; i < W; i++) {
-    if (isVisible(i, j)) count++;
-    score = Math.max(getScore(i, j), score);
-  }
+    for (let i = 0; i < W; i++) {
+        if (isVisible(i, j)) count++;
+        score = Math.max(getScore(i, j), score);
+    }
 }
 console.log(count);
 console.log(score);
