a656a4c890517435fa85dcb6fec5f5f03efc26bb
jackysee <jackysee@gmail.com>
2022-12-12 09:26:28 +0800

    2022 11

--- 2022/day11.ts
+++ 2022/day11.ts
@@ -2,71 +2,52 @@ import data from './day11_input.ts';
 // import data from './day11_sample.ts';
 
 type monkey = {
-    items: number[][];
+    items: number[];
     op: (n: number) => number;
     test: number;
     yes: number;
     no: number;
 };
-
 const M: monkey[] = [];
 const makeOp = (s: string) => {
     const [b, c] = s.split(' ').slice(-2);
-    if (b === '+') {
-        return (n: number) => n + Number(c);
-    }
-    if (b === '*') {
-        if (c === 'old') return (n: number) => n * n;
-        return (n: number) => n * Number(c);
-    }
-    return (n: number) => n;
+    if (b === '+') return (n: number) => n + +c;
+    if (c === 'old') return (n: number) => n * n;
+    return (n: number) => n * +c;
 };
-
+const nums = (s: string) => (s.match(/-?\d+/g) || []).map(Number);
 data()
     .split('\n\n')
     .forEach((l) => {
         const arr = l.split('\n');
-        const op = makeOp(arr[2]);
         M.push({
-            items: arr[1].match(/\d+/g)!.map((n) => [Number(n)]),
-            op,
-            test: arr[3].match(/\d+/g)!.map(Number)[0],
-            yes: arr[4].match(/\d+/g)!.map(Number)[0],
-            no: arr[5].match(/\d+/g)!.map(Number)[0]
+            items: nums(arr[1]),
+            op: makeOp(arr[2]),
+            test: nums(arr[3])[0],
+            yes: nums(arr[4])[0],
+            no: nums(arr[5])[0]
         });
     });
 
-//remainder
-M.forEach((m) => {
-    m.items = m.items.map(([i]: number[]) => {
-        return M.map((_m) => i % _m.test);
-    });
-});
-
-let p1Inspected = [...Array(M.length).fill(0)];
-let p2Inspected = [...Array(M.length).fill(0)];
-
-// console.log(M);
-// const ROUND = 20;
-const ROUND = 10000;
-for (let r = 0; r < ROUND; r++) {
-    M.forEach((m, i) => {
-        m.items.forEach((ns: number[]) => {
-            const _n = ns.map((n, i) => m.op(n) % M[i].test);
-            if (_n[i] === 0) {
-                M[m.yes].items.push(_n);
-            } else {
-                M[m.no].items.push(_n);
-            }
-            p2Inspected[i] += 1;
+function play(round: number, cb: (n: number, i: number) => number) {
+    const inspected = Array(M.length).fill(0);
+    const items = M.map((m) => m.items.map((n) => Array(M.length).fill(n)));
+    for (let r = 0; r < round; r++) {
+        M.forEach((m, i) => {
+            items[i].forEach((ns: number[]) => {
+                const _n = ns.map((n, i) => cb(m.op(n), i));
+                const to = _n[i] % m.test === 0 ? m.yes : m.no;
+                items[to].push(_n);
+                inspected[i] += 1;
+            });
+            items[i] = [];
         });
-        m.items = [];
-    });
-}
-//sample ans 2713310158
-console.log(
-    p2Inspected
+    }
+    return inspected
         .sort((a, b) => b - a)
         .slice(0, 2)
-        .reduce((a, c) => a * c, 1)
-);
+        .reduce((a, c) => a * c, 1);
+}
+
+console.log(play(20, (n) => Math.floor(n / 3)));
+console.log(play(10000, (n, i) => n % M[i].test));
