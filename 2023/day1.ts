089df565d221eff4007905fbd7e60d3c1a1955fb
jackysee <jackysee@gmail.com>
2023-12-01 15:46:09 +0000

    refactor

--- 2023/day1.ts
+++ 2023/day1.ts
@@ -1,6 +1,5 @@
 import data from './day1_input.ts';
 // import data from './day1_sample.ts';
-
 const lines = data().split('\n');
 const sum = (a: number, c: number) => a + c;
 const digitStr = 'one|two|three|four|five|six|seven|eight|nine';
@@ -9,13 +8,11 @@ const digitMap = digitStr.split('|').reduce((a, c, i) => {
     return a;
 }, {} as Record<string, string>);
 const toNumStr = (s: string) => digitMap[s] || s;
-const findValue =
-    (re: string) =>
-    (s: string): number => {
-        const first = s.match(new RegExp(re));
-        const last = s.match(new RegExp('.*(' + re + ')'));
-        if (!first || !last) return 0;
-        return Number(toNumStr(first[0]) + toNumStr(last[1]));
-    };
+const findValue = (re: string) => (s: string) => {
+    const first = s.match(new RegExp(re));
+    const last = s.match(new RegExp(`.*(${re})`));
+    if (!first || !last) return 0;
+    return Number(toNumStr(first[0]) + toNumStr(last[1]));
+};
 console.log('A', lines.map(findValue('\\d')).reduce(sum, 0));
-console.log('B', lines.map(findValue('\\d|' + digitStr)).reduce(sum, 0));
+console.log('B', lines.map(findValue(`\\d|${digitStr}`)).reduce(sum, 0));
