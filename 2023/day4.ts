50df5d79969bd7371f367d1c62a4c09dbd35aee3
jackysee <jackysee@gmail.com>
2023-12-04 10:53:35 +0000

    refactor

--- 2023/day4.ts
+++ 2023/day4.ts
@@ -8,11 +8,11 @@ data()
     .split('\n')
     .forEach((l, i) => {
         const numbers = l.split(': ')[1];
-        const [winning, scratchCards] = numbers
+        const [winning, card] = numbers
             .split(' | ')
             .map((s) => [...s.matchAll(/\d+/g)].map(Number));
         const winningSet = new Set(winning);
-        const matches = scratchCards.filter((n) => winningSet.has(n)).length;
+        const matches = card.filter((n) => winningSet.has(n)).length;
         if (matches > 0) A += Math.pow(2, matches - 1);
         for (let j = 1; j <= matches; j++) {
             won[i + j] = (won[i + j] || 0) + (won[i] || 0) + 1;
