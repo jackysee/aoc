67534f840f67ac5c268b6b9cb37bde62e2a34ca4
jackysee <jackysee@gmail.com>
2023-12-06 12:26:28 +0000

    2023 6

---
+++ 2023/day6.ts
@@ -0,0 +1,32 @@
+import data from './day6_input.ts';
+// import data from './day6_sample.ts';
+const input = data().split('\n');
+const times = [...input[0].matchAll(/\d+/g)].map(Number);
+const dists = [...input[1].matchAll(/\d+/g)].map(Number);
+
+const getWinCount = (dist: number, time: number) => {
+    let hold = 1;
+    let count = 0;
+    while (hold < time) {
+        const travel = (time - hold) * hold;
+        if (travel > dist) count++;
+        hold++;
+    }
+    return count;
+};
+
+console.log(
+    'A',
+    dists.map((d, i) => getWinCount(d, times[i])).reduce((a, c) => a * c, 1)
+);
+console.log('B', getWinCount(+dists.join(''), +times.join('')));
+
+const getCountQuadratic = (d: number, t: number) => {
+    // d = x*t - x*x
+    // -x*x + x*t - d = 0  => a = -1, b = t, c = -d
+    const sqrt = Math.sqrt(t * t - 4 * d);
+    const d1 = Math.ceil((t + sqrt) / 2);
+    const d2 = Math.floor((t - sqrt) / 2);
+    return d1 - d2 - 1;
+};
+console.log(getCountQuadratic(+dists.join(''), +times.join('')));
