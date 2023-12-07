33700bf7ba6949a2df77fc26083a5c9e229c1caf
jackysee <jackysee@gmail.com>
2023-12-07 16:52:40 +0000

    refactor

--- 2023/day7.ts
+++ 2023/day7.ts
@@ -12,70 +12,55 @@ const hands: hand[] = data()
 const cardRank = Object.fromEntries(
     '23456789TJQKA'.split('').map((c, i) => [c, i + 1])
 );
+const typeRank = Object.fromEntries(
+    '11111,2111,221,311,32,41,5'.split(',').map((c, i) => [c, i + 1])
+);
 
-const typeRank: Record<string, number> = {
-    '5': 7,
-    '41': 6,
-    '32': 5,
-    '311': 4,
-    '221': 3,
-    '2111': 2,
-    '11111': 1
-};
-
-const getLetterFreq = (s: string) => {
-    const freq: Record<string, number> = {};
-    s.split('').forEach((c) => (freq[c] = (freq[c] ?? 0) + 1));
-    return freq;
-};
-
-const getType = (cards: string) => {
-    return Object.values(getLetterFreq(cards))
-        .sort((a, b) => b - a)
-        .join('');
+const getTypeRank = (cards: string) => {
+    const freq = {} as Record<string, number>;
+    cards.split('').forEach((c) => (freq[c] = (freq[c] ?? 0) + 1));
+    return typeRank[
+        Object.values(freq)
+            .sort((a, b) => b - a)
+            .join('')
+    ];
 };
 
 const sortLabel = (a: string, b: string) => {
     for (let i = 0; i < a.length; i++) {
-        const d = cardRank[a[i]] - cardRank[b[i]];
+        const d = Math.sign(cardRank[a[i]] - cardRank[b[i]]);
         if (d === 0) continue;
-        return d < 0 ? -1 : 1;
+        return d;
     }
     return 0;
 };
 
-const sortType = (a: string, b: string) => {
-    const aRank = typeRank[getType(a)];
-    const bRank = typeRank[getType(b)];
-    if (aRank === bRank) return 0;
-    return aRank < bRank ? -1 : 1;
-};
+const sortType = (a: string, b: string) =>
+    Math.sign(getTypeRank(a) - getTypeRank(b));
 
-const sortStrength = (a: string, b: string) => {
-    const r = sortType(a, b);
-    return r === 0 ? sortLabel(a, b) : r;
-};
+const sortStrength = (a: string, b: string) =>
+    sortType(a, b) || sortLabel(a, b);
 
 const getWinning = (sortFunc: (a: hand, b: hand) => number) =>
     hands.sort(sortFunc).reduce((a, c, i) => a + c.bid * (i + 1), 0);
 
 console.log(
     'A',
-    getWinning((a: hand, b: hand) => sortStrength(a.cards, b.cards))
+    getWinning((a, b) => sortStrength(a.cards, b.cards))
 );
 
-const fillJoker = (s: string) => {
-    return s
+const fillJoker = (s: string) =>
+    s
         .split('')
         .map((c) => s.replaceAll('J', c))
         .sort((a, b) => sortStrength(b, a))[0];
-};
 
 cardRank.J = 0;
 console.log(
     'B',
-    getWinning((a: hand, b: hand) => {
-        const r = sortType(fillJoker(a.cards), fillJoker(b.cards));
-        return r === 0 ? sortLabel(a.cards, b.cards) : r;
-    })
+    getWinning(
+        (a, b) =>
+            sortType(fillJoker(a.cards), fillJoker(b.cards)) ||
+            sortLabel(a.cards, b.cards)
+    )
 );
--- get_puzzle.sh
+++ get_puzzle.sh
@@ -23,16 +23,6 @@ tee $1/day$2.ts << EOF
 import data from './day${2}_input.ts';
 // import data from './day${2}_sample.ts';
 
-const ints = (s: string) => (s.match(/-?\d+/g) || []).map(Number);
-const sum = (a: number, c: number) => a + c;
-const asc = (a: number, b: number) => a - b;
-const desc = (a: number, b: number) => b - a;
-// prettier-ignore
-const range = (a: number, b: number) => Array.from({length: b - a}, (_, n) => n + a);
-// prettier-ignore
-const DIRS = [[0,1],[0,-1],[1,0],[-1,0]];
-
-let arr: number[] = data().split('\n').map(Number);
 
 EOF
 
