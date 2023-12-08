9037805babb3899ff8b40e84c1aefddc7afa2d2a
jackysee <jackysee@gmail.com>
2023-12-08 10:06:25 +0000

    2023 8

---
+++ 2023/day8.ts
@@ -0,0 +1,40 @@
+import data from './day8_input.ts';
+// import data from './day8_sample.ts';
+const lines = data().split('\n');
+const steps = lines[0].split('');
+
+const M: Record<string, Record<string, string>> = {};
+lines.slice(2).forEach((l) => {
+    const [node, L, R] = l.match(/\w+/g)!;
+    M[node] = { L, R };
+});
+
+const findStep = (node: string, isDest: (a: string) => boolean) => {
+    let count = 0;
+    let i = 0;
+    while (true) {
+        node = M[node][steps[i]];
+        count++;
+        if (isDest(node)) break;
+        i++;
+        if (i === steps.length) i = 0;
+    }
+    return count;
+};
+
+console.log(
+    'A',
+    findStep('AAA', (n) => n === 'ZZZ')
+);
+
+function lcm(arr: number[]) {
+    const gcd = (x: number, y: number): number => (!y ? x : gcd(y, x % y));
+    const _lcm = (x: number, y: number) => (x * y) / gcd(x, y);
+    return arr.reduce((a, b) => _lcm(a, b));
+}
+
+const nodeSteps = Object.keys(M)
+    .filter((n) => /A$/.test(n))
+    .map((n) => findStep(n, (n) => /Z$/.test(n)));
+
+console.log('B', lcm(nodeSteps));
