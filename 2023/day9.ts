825ac1dfa66f8ee003933b0c87f6bee73836f40c
jackysee <jackysee@gmail.com>
2023-12-09 09:27:49 +0000

    refactor

--- 2023/day9.ts
+++ 2023/day9.ts
@@ -5,25 +5,23 @@ const input = data()
     .split('\n')
     .map((l) => l.split(' ').map(Number));
 
-const getPrevNext = (ns: number[]) => {
-    const lefts = [];
-    const rights = [];
+const getNext = (ns: number[]) => {
+    let sum = 0;
     while (!ns.every((n) => n === 0)) {
-        lefts.push(ns[0]);
-        rights.push(ns[ns.length - 1]);
+        sum += ns.at(-1)!;
         ns = ns.reduce((a, c, i) => {
             if (i === 0) return a;
             a.push(c - ns[i - 1]);
             return a;
         }, [] as number[]);
     }
-    return [
-        lefts.reduceRight((a, c) => c - a, 0),
-        rights.reduce((a, c) => a + c, 0)
-    ];
+    return sum;
 };
-const result = input
-    .map(getPrevNext)
-    .reduce((a, c) => [a[0] + c[1], a[1] + c[0]], [0, 0]);
-console.log('A', result[0]);
-console.log('B', result[1]);
+console.log(
+    'A',
+    input.map(getNext).reduce((a, c) => a + c, 0)
+);
+console.log(
+    'B',
+    input.map((ns) => getNext(ns.toReversed())).reduce((a, c) => a + c, 0)
+);
