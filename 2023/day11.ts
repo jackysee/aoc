ebbbfc1b33aba910b0b7fef1ed34a858e62e0be7
jackysee <jackysee@gmail.com>
2023-12-11 12:19:13 +0000

    refactor

--- 2023/day11.ts
+++ 2023/day11.ts
@@ -7,18 +7,16 @@ const lines = data().split('\n');
 lines.forEach((l, r) => {
     if (/^\.+$/.test(l)) emptyRow.push(r);
     l.split('').forEach((_n, c) => {
-        if (r === 0)
-            if (/^\.+$/.test(lines.map((l) => l[c]).join(''))) emptyCol.push(c);
+        if (r === 0 && /^\.+$/.test(lines.map((l) => l[c]).join('')))
+            emptyCol.push(c);
         if (_n === '#') stars.push([r, c]);
     });
 });
 
-const getPos = ([r, c]: number[], factor: number) => {
-    return [
-        r + emptyRow.filter((i) => i < r).length * (factor - 1),
-        c + emptyCol.filter((i) => i < c).length * (factor - 1)
-    ];
-};
+const getPos = ([r, c]: number[], factor: number) => [
+    r + emptyRow.filter((i) => i < r).length * (factor - 1),
+    c + emptyCol.filter((i) => i < c).length * (factor - 1)
+];
 
 const ans = (factor: number) => {
     let d = 0;
