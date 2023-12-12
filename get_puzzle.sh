bfb33499bc6832aca1bf5fb186a89047ecdaab97
jackysee <jackysee@gmail.com>
2023-12-12 17:30:16 +0000

    2023 12

--- get_puzzle.sh
+++ get_puzzle.sh
@@ -22,7 +22,7 @@ echo "Data file written to $1/day$2_input.ts"
 tee $1/day$2.ts << EOF
 import data from './day${2}_input.ts';
 // import data from './day${2}_sample.ts';
-const lines = data.split('\n');
+const lines = data().split('\n');
 
 
 EOF
