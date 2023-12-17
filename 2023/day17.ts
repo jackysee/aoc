af59b1bf7e06ee1453094cf3d1c593ce99ecf7d0
jackysee <jackysee@gmail.com>
2023-12-17 21:54:23 +0000

    2023 17 B

--- 2023/day17.ts
+++ 2023/day17.ts
@@ -1,6 +1,7 @@
 import { BinaryHeap } from 'https://deno.land/x/collections@0.12.1/mod.ts';
-// import data from './day17_input.ts';
-import data from './day17_sample.ts';
+// import BinaryHeap from '../util/binaryHeap.ts';
+import data from './day17_input.ts';
+// import data from './day17_sample.ts';
 const M = data()
     .split('\n')
     .map((l) => [...l].map(Number));
@@ -9,81 +10,51 @@ type State = {
     r: number;
     c: number;
     d: string;
-    st: number;
+    step: number;
     heat: number;
 };
 
-const key = (s: State) => [s.r, s.c, s.d, s.st].join(',');
+const key = (s: State) => [s.r, s.c, s.d, s.step].join(',');
 
 const goStraight = (s: State) => {
-    let { r, c, d, st } = s;
+    let { r, c, d, step } = s;
     if (d === 'E') c++;
     if (d === 'N') r--;
     if (d === 'S') r++;
     if (d === 'W') c--;
-    return { r, c, d, st: st + 1 };
+    return { r, c, d, step: step + 1 };
 };
 
 const turnLeft = (s: State) => {
-    let { r, c, d } = s;
-    if (d === 'E') {
-        r--;
-        d = 'N';
-    } else if (d === 'N') {
-        c--;
-        d = 'W';
-    } else if (d === 'W') {
-        r++;
-        d = 'S';
-    } else if (d === 'S') {
-        c++;
-        d = 'E';
-    }
-    return { r, c, d, st: 1 };
+    const d = { E: 'N', N: 'W', W: 'S', S: 'E' }[s.d]!;
+    return goStraight({ ...s, d, step: 0 });
 };
 
 const turnRight = (s: State) => {
-    let { r, c, d } = s;
-    if (d === 'E') {
-        r++;
-        d = 'S';
-    } else if (d === 'S') {
-        c--;
-        d = 'W';
-    } else if (d === 'W') {
-        r--;
-        d = 'N';
-    } else if (d === 'N') {
-        c++;
-        d = 'E';
-    }
-    return { r, c, d, st: 1 };
+    const d = { E: 'S', S: 'W', W: 'N', N: 'E' }[s.d]!;
+    return goStraight({ ...s, d, step: 0 });
 };
 
 const findHeat = (ultra = false) => {
-    const queue: BinaryHeap<State> = new BinaryHeap<State>(
-        (a, b) => a.heat - b.heat
-    );
-    queue.push({ r: 0, c: 0, d: 'E', st: 0, heat: 0 });
-    queue.push({ r: 0, c: 0, d: 'S', st: 0, heat: 0 });
+    const queue = new BinaryHeap<State>((a, b) => a.heat - b.heat);
+    queue.push({ r: 0, c: 0, d: 'E', step: 0, heat: 0 });
+    queue.push({ r: 0, c: 0, d: 'S', step: 0, heat: 0 });
     const seen = new Set<string>();
-    // const seen:Record<string,number>  = {}
     while (queue.length) {
         const pos = queue.pop()!;
         if (pos.r === M.length - 1 && pos.c === M[0].length - 1) {
             if (!ultra) return pos.heat;
-            if (ultra && pos.st >= 4) return pos.heat;
+            if (ultra && pos.step >= 4) return pos.heat;
         }
         if (seen.has(key(pos))) continue;
         seen.add(key(pos));
         for (const p of [goStraight(pos), turnLeft(pos), turnRight(pos)]) {
             if (M[p.r]?.[p.c] === undefined) continue;
             if (ultra) {
-                if (p.st > 10) continue;
-                if (pos.d !== p.d && p.st < 4 && pos.r !== 0 && pos.c !== 0)
-                    continue;
+                if (p.step > 10) continue;
+                if (pos.d !== p.d && pos.step < 4) continue;
             } else {
-                if (p.st > 3) continue;
+                if (p.step > 3) continue;
             }
             queue.push({
                 ...p,
@@ -92,5 +63,6 @@ const findHeat = (ultra = false) => {
         }
     }
 };
-// console.log('A', findHeat());
+
+console.log('A', findHeat());
 console.log('B', findHeat(true));
