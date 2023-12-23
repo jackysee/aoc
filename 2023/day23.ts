ff76f6ff58d2fee82e051061cef11e616782b37b
jackysee <jackysee@gmail.com>
2023-12-23 12:04:19 +0000

    2023 23 B

--- 2023/day23.ts
+++ 2023/day23.ts
@@ -1,11 +1,10 @@
 import data from './day23_input.ts';
 // import data from './day23_sample.ts';
-// import { BinaryHeap } from '../util/binaryHeap.ts';
 const M = data()
     .split('\n')
     .map((l) => [...l]);
 
-const adjs = (r, c) => {
+const neighbors = (r: number, c: number, climb = false) => {
     const result = [];
     let dir = [
         [0, 1],
@@ -14,10 +13,12 @@ const adjs = (r, c) => {
         [-1, 0]
     ];
     const tile = M[r][c];
-    if (tile === '<') dir = [[0, -1]];
-    if (tile === '>') dir = [[0, 1]];
-    if (tile === '^') dir = [[-1, 0]];
-    if (tile === 'v') dir = [[1, 0]];
+    if (!climb) {
+        if (tile === '<') dir = [[0, -1]];
+        if (tile === '>') dir = [[0, 1]];
+        if (tile === '^') dir = [[-1, 0]];
+        if (tile === 'v') dir = [[1, 0]];
+    }
     for (const [dr, dc] of dir) {
         const [nr, nc] = [r + dr, c + dc];
         const tile = M[nr]?.[nc];
@@ -27,23 +28,74 @@ const adjs = (r, c) => {
 };
 
 const dest = [M.length - 1, M[0].length - 2];
-let result = 0;
-const dfs = (current: number[], path: number[][], seen: Set<string>) => {
+const toKey = (r: number, c: number) => [r, c].join(',');
+const dfs = (
+    current: number[],
+    seen: Set<string>,
+    dist: number,
+    adjs: (r: number, c: number) => number[][]
+) => {
     if (current[0] === dest[0] && current[1] === dest[1]) {
-        result = Math.max(result, path.length);
+        max = Math.max(max, dist);
     }
     const [r, c] = current;
-    for (const n of adjs(r, c)) {
-        const key = `${r},${c}`;
+    for (const [nr, nc, d = 1] of adjs(r, c)) {
+        const key = toKey(r, c);
         if (!seen.has(key)) {
-            path.push(n);
             seen.add(key);
-            dfs(n, path, seen);
+            dfs([nr, nc], seen, dist + d, adjs);
             seen.delete(key);
-            path.pop();
         }
     }
 };
 
-dfs([0, 1], [], new Set());
-console.log('done', result);
+let max = 0;
+dfs([0, 1], new Set(), 0, neighbors);
+console.log('A', max);
+
+const V = new Set<string>();
+M.forEach((l, r) => {
+    l.forEach((t, c) => {
+        if (t !== '#' && neighbors(r, c, true).length > 2) {
+            V.add(`${r},${c}`);
+        }
+    });
+});
+V.add('0,1');
+V.add(dest.join(','));
+
+const P: Record<string, number[][]> = {};
+for (const s of V) {
+    const [r, c] = s.split(',').map(Number);
+    let queue = [[r, c]];
+    const seen = new Set();
+    seen.add([r, c] + '');
+    let dist = 0;
+    const key = toKey(r, c);
+    while (queue.length) {
+        const _q: number[][] = [];
+        dist += 1;
+        for (const v of queue) {
+            for (const a of neighbors(v[0], v[1], true)) {
+                const ak = a + '';
+                if (!seen.has(ak)) {
+                    seen.add(ak);
+                    if (V.has(ak)) {
+                        P[key] = P[key] || [];
+                        P[key].push([...a, dist]);
+                    } else {
+                        _q.push(a);
+                    }
+                }
+            }
+        }
+        queue = _q;
+    }
+}
+
+max = 0;
+const adj2 = (r: number, c: number) => P[toKey(r, c)] || [];
+console.time('B');
+dfs([0, 1], new Set(), 0, adj2);
+console.log('B', max);
+console.timeEnd('B');
