10ed4d31346dd6b76f7d6fe76096b1dbef5c7828
jackysee <jackysee@gmail.com>
2024-11-29 19:10:07 +0000

    z3 solver

--- 2023/day24.ts
+++ 2023/day24.ts
@@ -5,10 +5,13 @@ const H = data()
     .split('\n')
     .map((l) => [...l.match(/-?\d+/g)!].map(Number));
 
-const intersectXY = (l1: number[], l2: number[]) => {
-    const [x1, y1, _z1, vx1, vy1, _vz1] = l1;
-    const [x2, y2, _z2, vx2, vy2, _vz2] = l2;
-    //y = ax + c , y = bx + d
+const intersect = (l1: number[], l2: number[], type = 'xy') => {
+    let [x1, y1, z1, vx1, vy1, vz1] = l1;
+    let [x2, y2, z2, vx2, vy2, vz2] = l2;
+    if (type === 'xz') {
+        [y1, vy1] = [z1, vz1];
+        [y2, vy2] = [z2, vz2];
+    }
     const a = (y1 + vy1 - y1) / (x1 + vx1 - x1);
     const c = y1 - a * x1;
     const b = (y2 + vy2 - y2) / (x2 + vx2 - x2);
@@ -17,16 +20,23 @@ const intersectXY = (l1: number[], l2: number[]) => {
     if (a === b) return undefined;
     const x = (d - c) / (a - b);
     const y = a * ((d - c) / (a - b)) + c;
-    if (vx1 > 0 && x < x1) return undefined;
-    if (vx1 < 0 && x > x1) return undefined;
-    if (vy1 > 0 && y < y1) return undefined;
-    if (vy1 < 0 && y > y1) return undefined;
-    if (vx2 > 0 && x < x2) return undefined;
-    if (vx2 < 0 && x > x2) return undefined;
-    if (vy2 > 0 && y < y2) return undefined;
-    if (vy2 < 0 && y > y2) return undefined;
     return [x, y];
 };
+
+//const intersectXY = (l1: number[], l2: number[]) => {
+//    const [x1, y1, _z1, vx1, vy1, _vz1] = l1;
+//    const [x2, y2, _z2, vx2, vy2, _vz2] = l2;
+//    //y = ax + c , y = bx + d
+//    const a = (y1 + vy1 - y1) / (x1 + vx1 - x1);
+//    const c = y1 - a * x1;
+//    const b = (y2 + vy2 - y2) / (x2 + vx2 - x2);
+//    const d = y2 - b * x2;
+
+//    if (a === b) return undefined;
+//    const x = (d - c) / (a - b);
+//    const y = a * ((d - c) / (a - b)) + c;
+//    return [x, y];
+//};
 
 const inside = ([x, y]: number[], [b1, b2]: number[]) => {
     return x >= b1 && x <= b2 && y >= b1 && y <= b2;
@@ -37,14 +47,68 @@ let count = 0;
 const B = [200000000000000, 400000000000000];
 H.forEach((a, i) => {
     H.slice(i + 1).forEach((b) => {
-        const pt = intersectXY(a, b);
-        if (pt && inside(pt, B)) count++;
+        const pt = intersect(a, b);
+        if (!pt) return;
+        if (!inside(pt!, B)) return;
+        const [x, _y] = pt;
+        if ((x < a[0] && a[3] > 0) || (x > a[0] && a[3] < 0)) return;
+        if ((x < b[0] && b[3] > 0) || (x > b[0] && b[3] < 0)) return;
+        count++;
     });
 });
 console.log('A', count);
-console.log('B', "deno has error with npm:z3-solver, use python instead :(")
 
+const SUB = H.slice(0, 10);
+const findCommonIntersection = (v: number[], type = 'xy') => {
+    let possible = true;
+    let current: number[] | undefined = undefined;
+    const lines = SUB.map((line) => {
+        const _line = [...line];
+        if (type === 'xy') {
+            _line[3] += v[0];
+            _line[4] += v[1];
+        }
+        if (type === 'xz') {
+            _line[3] += v[0];
+            _line[5] += v[1];
+        }
+        return _line;
+    });
+    lines.forEach((a, i) => {
+        lines.slice(i + 1).forEach((b) => {
+            if (!possible) return false;
+            const pt = intersect(a, b, type);
+            if (pt === undefined) return;
+            if (!current) current = pt;
+            possible = pt[0] === current[0] && pt[1] === current[1];
+        });
+    });
+    if (!possible) return false;
+    return current;
+};
 
+outer: for (let a = 0; a <= Infinity; a++) {
+    for (let b = 0; b <= a; b++) {
+        //prettier-ignore
+        for (const [x, y] of [ [a, b], [b, a] ]) {
+            //prettier-ignore
+            for (const [dx, dy] of [ [1, 1], [1, -1], [-1, 1], [-1, -1] ]) {
+                const xy = findCommonIntersection([x * dx, y * dy], 'xy');
+                if (!xy) continue;
+
+                for (let z = 0; z <= Infinity; z++) {
+                    for (const dz of [1, -1]) {
+                        console.log('try z', xy)
+                        const xz = findCommonIntersection([x * dx, z * dz], 'xz');
+                        if (!xz) continue;
+                        console.log(xy[0] + xy[1] + xz[1]);
+                        break outer;
+                    }
+                }
+            }
+        }
+    }
+}
 
 //import { init } from 'z3-solver';
 //import data from './day24_input.js';
@@ -79,4 +143,3 @@ console.log('B', "deno has error with npm:z3-solver, use python instead :(")
 //await solver.check();
 //console.log('B', Number(solver.model().eval(x.add(y).add(z)).value()));
 //console.timeEnd('B');
-
