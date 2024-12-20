dcc7758dc0caf1162dc49cdd4f81c6bf09520e8d
jackysee <jackysee@gmail.com>
2024-12-20 18:32:05 +0000

    refactor

--- 2024/day20.js
+++ 2024/day20.js
@@ -1,23 +1,22 @@
 import data from './day20_input.js';
 // import data from './day20_sample.js';
-const M = data()
-    .split('\n')
-    .map((l) => [...l]);
-
 let start;
 let end;
-M.forEach((l, r) =>
-    l.forEach((t, c) => {
-        if (t === 'S') {
-            start = [r, c];
-            M[r][c] = '.';
-        }
-        if (t === 'E') {
-            end = [r, c];
-            M[r][c] = '.';
-        }
-    })
-);
+const M = data()
+    .split('\n')
+    .map((l, r) => {
+        return [...l].map((m, c) => {
+            if (m === 'S') {
+                start = [r, c];
+                return '.';
+            }
+            if (m === 'E') {
+                end = [r, c];
+                return '.';
+            }
+            return m;
+        });
+    });
 
 const findTrack = () => {
     const result = [start];
@@ -41,16 +40,16 @@ const trackIndex = Object.fromEntries(
     Object.entries(track).map(([k, v]) => [v + '', +k])
 );
 
-const findCheatSaved = (r, c, d) => {
+const countCheatsByPos = (r, c, d, minSaved) => {
     const i = trackIndex[[r, c]];
-    const result = [];
+    let result = 0;
     for (let nr = r - d; nr <= r + d; nr++) {
         for (let nc = c - d; nc <= c + d; nc++) {
             const dd = Math.abs(nr - r) + Math.abs(nc - c);
             const ni = trackIndex[[nr, nc]];
             const saved = ni - i - dd;
-            if (dd <= d && M[nr]?.[nc] === '.' && saved > 0) {
-                result.push(saved);
+            if (dd <= d && M[nr]?.[nc] === '.' && saved >= minSaved) {
+                result++;
             }
         }
     }
@@ -58,20 +57,7 @@ const findCheatSaved = (r, c, d) => {
 };
 
 const countCheats = (t) => {
-    const saved = {};
-    track.forEach(([r, c]) => {
-        const cheats = findCheatSaved(r, c, t);
-        cheats.forEach((ds) => {
-            saved[ds] = saved[ds] ?? 0;
-            saved[ds]++;
-        });
-    });
-    let sum = 0;
-    Object.entries(saved).map(([saved, count]) => {
-        if (+saved >= 100) sum += count;
-    });
-
-    return sum;
+    return track.reduce((a, c) => a + countCheatsByPos(...c, t, 100), 0);
 };
 
 console.log('A', countCheats(2));
