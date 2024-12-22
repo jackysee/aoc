b3cc293ca17883a018ab6fe1b7c0075804fad8e7
jackysee <jackysee@gmail.com>
2024-12-22 10:18:59 +0000

    2024 22 B

--- 2024/day22.js
+++ 2024/day22.js
@@ -2,9 +2,7 @@ import data from './day22_input.js';
 // import data from './day22_sample.js';
 const ns = data().split('\n').map(Number);
 
-const mix = (n, m) => {
-    return ((n ^ m) >>> 0) % 16777216;
-};
+const mix = (n, m) => ((n ^ m) >>> 0) % 16777216;
 
 const generate = (n) => {
     n = mix(n * 64, n);
@@ -13,14 +11,29 @@ const generate = (n) => {
     return n;
 };
 
-const nthSecret = (num, n) => {
-    for (let i = 0; i < n; i++) {
+const M = {};
+let sum = 0;
+ns.forEach((num) => {
+    const prices = [];
+    const seen = {};
+    for (let i = 0; i < 2000; i++) {
         num = generate(num);
+        const p = num % 10;
+        prices.push(p);
+        if (i >= 4) {
+            const trend = [
+                prices[i - 3] - prices[i - 4],
+                prices[i - 2] - prices[i - 3],
+                prices[i - 1] - prices[i - 2],
+                prices[i] - prices[i - 1]
+            ].join(',');
+            if (!seen[trend]) {
+                M[trend] = (M[trend] ?? 0) + p;
+                seen[trend] = true;
+            }
+        }
     }
-    return num;
-};
-
-console.log(
-    'A',
-    ns.reduce((a, n) => a + nthSecret(n, 2000), 0)
-);
+    sum += num;
+});
+console.log('A', sum);
+console.log('B', Math.max(...Object.values(M)));
