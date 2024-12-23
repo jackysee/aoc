b0ecfdbc69cd56cac7ea3f2d73ed52d93c4fd36b
jackysee <jackysee@gmail.com>
2024-12-23 12:59:09 +0000

    2024 23 B

--- 2024/day23.js
+++ 2024/day23.js
@@ -11,7 +11,7 @@ data()
         M[c2].push(c1);
     });
 
-console.log(M);
+// console.log(Math.max(...Object.values(M).map((l) => l.length)));
 
 const setOfThree = new Set(
     Object.keys(M).flatMap((a) => {
@@ -24,5 +24,27 @@ const setOfThree = new Set(
     })
 );
 
-console.log(setOfThree);
-console.log([...setOfThree].filter((s) => /t[^,]/.test(s)).length);
+console.log('A', [...setOfThree].filter((s) => /t[^,]/.test(s)).length);
+
+const cliques = [];
+const findCliques = (P, R = new Set(), X = new Set()) => {
+    if (P.size === 0 && X.size === 0) {
+        cliques.push([...R].sort());
+        return 1;
+    }
+    let c = 0;
+    [...P].forEach((n) => {
+        const V = new Set([n]);
+        const N = new Set(M[n]);
+        c += findCliques(P.intersection(N), R.union(V), X.intersection(N));
+        P = P.difference(V);
+        X = X.union(V);
+    });
+    return c;
+};
+
+findCliques(new Set(Object.keys(M)));
+
+const maxLen = Math.max(...cliques.map((a) => a.length));
+const cs = cliques.filter((a) => a.length === maxLen);
+console.log('B', cs[0].join(','));
