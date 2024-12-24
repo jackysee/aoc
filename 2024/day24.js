56f915961373e82e2a04f82848d33275bea2db70
jackysee <jackysee@gmail.com>
2024-12-24 12:02:33 +0000

    2024 24 B

--- 2024/day24.js
+++ 2024/day24.js
@@ -1,48 +1,78 @@
 import data from './day24_input.js';
 // import data from './day24_sample.js';
 
-const list = data().split('\n\n');
-const values = list[0]
-    .split('\n')
-    .map((l) => l.split(': ').map((l, i) => (i === 1 ? +l : l)));
-const conns = list[1].split('\n').map((l) => l.split(/ -> | /));
-
-// console.log(conns);
-// console.log(values);
+const ds = data().split('\n\n');
+const values = Object.fromEntries(
+    ds[0]
+        .split('\n')
+        .map((l) => l.split(': ').map((l, i) => (i === 1 ? +l : l)))
+);
+const conns = ds[1].split('\n').map((l) => {
+    const [w1, op, w2, out] = l.split(/ -> | /);
+    return { w1, op, w2, out };
+});
 
 const zs = conns
-    .filter((a) => a[3].startsWith('z'))
-    .map((a) => a[3])
+    .filter((a) => a.out.startsWith('z'))
+    .map((a) => a.out)
     .sort()
     .reverse();
-console.log(zs);
-let currValues = values.map((l) => [...l]);
-const findValue = (w) => currValues.find((v) => v[0] === w)?.[1];
+
 const getValue = (v1, v2, op) => {
-    if (v1 === undefined || v2 === undefined) return undefined;
     if (op === 'AND') return v1 && v2;
     if (op === 'OR') return v1 || v2;
     if (op === 'XOR') return v1 !== v2 ? 1 : 0;
 };
 
-// let i = 0;
-// let changed = false;
-while (true) {
-    // const newValues = [];
-    conns.forEach(([w1, op, w2, output]) => {
-        const v1 = findValue(w1);
-        const v2 = findValue(w2);
-        if (v1 !== undefined && v2 !== undefined) {
-            currValues.push([output, getValue(v1, v2, op)]);
+const run = () => {
+    const W = { ...values };
+    while (true) {
+        conns.forEach(({ w1, op, w2, out }) => {
+            const v1 = W[w1];
+            const v2 = W[w2];
+            if (v1 !== undefined && v2 !== undefined) {
+                W[out] = getValue(v1, v2, op);
+            }
+        });
+        if (zs.every((z) => W[z] !== undefined)) {
+            break;
         }
-    });
-    // currValues = newValues;
-    if (zs.every((z) => findValue(z) !== undefined)) {
-        console.log('--->', zs.map((z) => findValue(z)).join(''));
-        break;
-        // i++;
-        // if (i > 100) break;
     }
+    return parseInt(zs.map((z) => W[z]).join(''), 2);
+};
+
+const result = run();
+console.log('A', result);
+
+const list = [];
+for (let i = 0; i < zs.length; i++) {
+    const id = ('' + i).padStart(2, '0');
+    const isXYIn = (c) =>
+        (c.w1 === `x${id}` && c.w2 === `y${id}`) ||
+        (c.w1 === `y${id}` && c.w2 === `x${id}`);
+    const xor1 = conns.find((c) => isXYIn(c) && c.op === 'XOR');
+    const and1 = conns.find((c) => isXYIn(c) && c.op === 'AND');
+    const z = conns.find((c) => c.out === `z${id}`);
+
+    if (xor1 === undefined || and1 === undefined || z === undefined) continue;
+
+    if (z.op !== 'XOR') list.push(z.out);
+
+    const or = conns.find((c) => c.w1 === and1.out || c.w2 === and1.out);
+    if (or && or.op !== 'OR' && i > 0) list.push(and1.out);
+
+    const after = conns.find((c) => c.w1 === xor1.out || c.w2 === xor1.out);
+    if (after?.op === 'OR') list.push(xor1.out);
 }
-// console.log(currValues, zs.map((z) => findValue(z)).join(''));
-console.log(parseInt(zs.map((z) => findValue(z)).join(''), 2));
+list.push(
+    ...conns
+        .filter(
+            (c) =>
+                !/^[xy]/.test(c.w1) &&
+                !/^[xy]/.test(c.w2) &&
+                !/^z/.test(c.out) &&
+                c.op === 'XOR'
+        )
+        .map((c) => c.out)
+);
+console.log('B', list.sort().join(','));
