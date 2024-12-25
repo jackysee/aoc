3d07aef8e181122b7f39d9dffd753f8d0aa5edb6
jackysee <jackysee@gmail.com>
2024-12-25 07:01:53 +0000

    refactor

--- 2024/day25.js
+++ 2024/day25.js
@@ -2,25 +2,22 @@ import data from './day25_input.js';
 // import data from './day25_sample.js';
 const keys = [];
 const locks = [];
-data()
-    .split('\n\n')
-    .forEach((d) => {
-        const m = d.split('\n');
-        const cols = [0, 1, 2, 3, 4].map(
-            (c) =>
-                m
-                    .map((r) => r[c])
-                    .join('')
-                    .match(/#+/)[0].length - 1
-        );
-        if (m[0] === '#####') locks.push(cols);
-        if (m.at(-1) === '#####') keys.push(cols);
-    });
+for (const d of data().split('\n\n')) {
+    const m = d.split('\n');
+    const cols = [0, 1, 2, 3, 4].map(
+        (c) =>
+            m
+                .map((r) => r[c])
+                .join('')
+                .match(/#+/)[0].length - 1
+    );
+    if (m[0] === '#####') locks.push(cols);
+    if (m.at(-1) === '#####') keys.push(cols);
+}
 
 let fit = 0;
-keys.forEach((ks) => {
-    locks.forEach((ls) => {
+for (const ks of keys)
+    for (const ls of locks) {
         if (ls.every((l, i) => l + ks[i] <= 5)) fit += 1;
-    });
-});
+    }
 console.log(fit);
