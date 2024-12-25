d83429d4c2a2f41519c302213f41758ab4932202
jackysee <jackysee@gmail.com>
2024-12-25 06:41:12 +0000

    refactor

--- 2024/day25.js
+++ 2024/day25.js
@@ -1,24 +1,22 @@
 import data from './day25_input.js';
 // import data from './day25_sample.js';
-
-const getCols = (m) => {
-    return [0, 1, 2, 3, 4].map(
-        (c) =>
-            m
-                .map((r) => r[c])
-                .join('')
-                .match(/#+/)[0].length - 1
-    );
-};
 const keys = [];
 const locks = [];
 data()
     .split('\n\n')
     .forEach((d) => {
         const m = d.split('\n');
-        if (m[0] === '#####') locks.push(getCols(m));
-        if (m.at(-1) === '#####') keys.push(getCols(m));
+        const cols = [0, 1, 2, 3, 4].map(
+            (c) =>
+                m
+                    .map((r) => r[c])
+                    .join('')
+                    .match(/#+/)[0].length - 1
+        );
+        if (m[0] === '#####') locks.push(cols);
+        if (m.at(-1) === '#####') keys.push(cols);
     });
+
 let fit = 0;
 keys.forEach((ks) => {
     locks.forEach((ls) => {
