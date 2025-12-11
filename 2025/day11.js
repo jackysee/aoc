601430a17a0b7f227cc3375495f37d21a5013ab8
jackysee <jackysee@gmail.com>
2025-12-11 12:16:51 +0000

    2025 11

---
+++ 2025/day11.js
@@ -0,0 +1,27 @@
+import data from './day11_input.js';
+// import { data } from './day11_sample.js';
+// import { data2 as data } from './day11_sample.js';
+const M = data()
+    .split('\n')
+    .reduce((a, c) => {
+        const [key, value] = c.split(': ');
+        a[key] = value.split(' ');
+        return a;
+    }, {});
+
+const dfs = (from, to, memo = {}) => {
+    if (from === to) return 1;
+    if (memo[from] !== undefined) return memo[from];
+    memo[from] = (M[from] || []).reduce((a, c) => a + dfs(c, to, memo), 0);
+    return memo[from];
+};
+
+console.log(dfs('you', 'out'));
+
+const countPaths = (paths) =>
+    paths.slice(0, -1).reduce((a, c, i) => a * dfs(c, paths[i + 1]), 1);
+
+console.log(
+    countPaths(['svr', 'fft', 'dac', 'out']) +
+        countPaths(['svr', 'dac', 'fft', 'out'])
+);
---
+++ 2025/day11_sample.js
@@ -0,0 +1,30 @@
+export const data = () =>
+    `
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+`.trim();
+
+export const data2 = () =>
+    `
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+`.trim();
